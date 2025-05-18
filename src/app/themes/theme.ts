5260373282fc12edbf119a6f7250527549495414
MohsenBg <moh.1380.1393@gmail.com>
2025-05-18 10:02:40 +0330

    feat: add custom theme, primeflex, primeicons, @primeng/themes, and primeng packages

---
+++ src/app/themes/theme.ts
@@ -0,0 +1,23 @@
+
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+import { ThemeType } from 'primeng/config';
+
+
+// لینک زیر برای مشاهده تنظیمات و مستندات تم است:
+// https://primeng.org/theming
+
+
+export const customTheme: ThemeType = {
+
+  preset: definePreset(Aura, {
+    // در این قسمت می‌توانید رنگ‌ها یا تنظیمات دلخواه تم را اضافه و سفارشی کنید
+
+  }),
+
+
+  options: {
+    // گزینه‌های اضافی برای تنظیمات تم
+
+  }
+};
