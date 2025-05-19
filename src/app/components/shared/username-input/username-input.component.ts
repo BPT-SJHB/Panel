9d1aba993ca1c1d731531e6c892fa651946a7159
MohsenBg <moh.1380.1393@gmail.com>
2025-05-19 07:48:25 +0330

    feat: add username-input component

---
+++ src/app/components/shared/username-input/username-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  imports: [],
+  templateUrl: './username-input.component.html',
+  styleUrl: './username-input.component.scss'
+})
+export class UsernameInputComponent {
+
+}
