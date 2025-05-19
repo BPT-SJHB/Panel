daac52b5ac8eb1c8aad04a44edd4a503d2a6efbd
MohsenBg <moh.1380.1393@gmail.com>
2025-05-19 07:48:25 +0330

    feat: add captcha-input component

---
+++ src/app/components/shared/captcha-input/captcha-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captcha-input',
+  imports: [],
+  templateUrl: './captcha-input.component.html',
+  styleUrl: './captcha-input.component.scss'
+})
+export class CaptchaInputComponent {
+
+}
