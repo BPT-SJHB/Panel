360ac0783abd63fccf3924d75d35fb689ff43271
rezishon <rezashahmoradi.81@outlook.com>
2025-05-20 17:34:03 +0330

    feat(services)[auth-user]: create `auth-user` service
    This service will help us authenticate the user

---
+++ src/app/services/AuthUser/auth-user.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IAuthUserValues } from './IAuthUserValues';
+import { IAuthUserApiRequest } from './IAuthUserApiRequest';
+import { IAuthUserApiRespond } from './IAuthUserApiRespond';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthUserService {}
