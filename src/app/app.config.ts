9476e2703d645de885eaf88579ef85a6c38e0c41
MohsenBg <moh.1380.1393@gmail.com>
2025-05-24 12:46:01 +0330

    feat: add (pwa) progressive web app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,6 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 
 import { routes } from './app.routes';
 import { customTheme } from './themes/theme';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +22,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({ theme: customTheme }),
     importProvidersFrom(ToastModule),
     MessageService,
-    CookieService
+    CookieService, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
