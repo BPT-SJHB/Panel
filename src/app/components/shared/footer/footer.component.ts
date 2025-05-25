54bc1b50eb20b244e7763f8e9af88f7b73945f09
rezishon <rezashahmoradi.81@outlook.com>
2025-05-25 14:01:09 +0330

    feat(components/shared)[footer]: create `footer` component
    - This component will help users. It contains ticket part, phone numbers
    which are be able to copy to users clipboard.

---
+++ src/app/components/shared/footer/footer.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { PopoverModule } from 'primeng/popover';
+import { DialogModule } from 'primeng/dialog';
+import { TextareaModule } from 'primeng/textarea';
+import { UsernameInputComponent } from '../inputs/username-input/username-input.component';
+import { PasswordInputComponent } from '../inputs/password-input/password-input.component';
+import { CaptchaInputComponent } from '../inputs/captcha-input/captcha-input.component';
+import { UserAuthService } from 'app/services/user-auth-service/user-auth.service';
+import { ToastService } from 'app/services/toast-service/toast.service';
+import { TextAreaInputComponent } from '../inputs/text-area-input/text-area-input.component';
+import { APP_ROUTES } from 'app/constants/routes';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  imports: [
+    CardModule,
+    ButtonModule,
+    DividerModule,
+    PopoverModule,
+    DialogModule,
+    UsernameInputComponent,
+    PasswordInputComponent,
+    CaptchaInputComponent,
+    TextareaModule,
+    TextAreaInputComponent,
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {
+}
