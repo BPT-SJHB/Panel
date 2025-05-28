15a988884fe06aea0c5d4c366050e6a4b9ad5d12
rezishon <rezashahmoradi.81@outlook.com>
2025-05-28 17:33:07 +0330

    chore(environments)[environment.development]: change the API ip

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,10 @@
-const apiBaseUrl = "http://192.168.1.2";
-const apiPort = 81;
+const apiBaseUrl = 'http://192.168.1.2';
+// const apiPort = 81;
 
 export const environment = {
   production: false,
-  apiUrl: `${apiBaseUrl}:${apiPort}`,
+  // apiUrl: `${apiBaseUrl}:${apiPort}`,
+  apiUrl: `${apiBaseUrl}`,
   // این قسمت برای من هست که دسترسی به api ندارم
   // اکه فعال کنی از api استفاده نمی کنه
   disableApi: false,
