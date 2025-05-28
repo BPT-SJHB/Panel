ed1ebb53853965c3e2654a29c613f31ee99bc228
rezishon <rezashahmoradi.81@outlook.com>
2025-05-28 18:07:26 +0330

    fix(data/mock)[cargo-terminal]: update the API mocking data

--- src/app/data/mock/cargo-terminal.mock.ts
+++ src/app/data/mock/cargo-terminal.mock.ts
@@ -1,64 +1,40 @@
-import { CargoTerminal } from "../model/cargo-terminal.model";
+import { LoadAnnouncementPlace } from 'app/data/model/load-announcement-place.model';
 
-export const mockCargoTerminals: CargoTerminal[] = [
+export const mockCargoTerminals: LoadAnnouncementPlace[] = [
   {
-    name: 'پایانه باربری شهید رجایی',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، جاده امیرکبیر، جنب ترمینال شهید رجایی',
+    LAPTitle: 'اعلام بار پایانه امیرکبیر',
+    LAPIconName: 'x1',
+    LAPURL: ' ',
+    Description: ' ',
   },
   {
-    name: 'پایانه باربری سپاهان',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، سپاهان‌شهر، شهرک صنعتی',
+    LAPTitle: 'اعلام بار پایانه مبارکه',
+    LAPIconName: 'x2',
+    LAPURL: ' ',
+    Description: ' ',
   },
   {
-    name: 'پایانه باربری شرق اصفهان',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، خروجی شرق، جاده نائین',
+    LAPTitle: 'اعلام بار پایانه زرین شهر',
+    LAPIconName: 'x3',
+    LAPURL: ' ',
+    Description: ' ',
   },
   {
-    name: 'پایانه باربری جهان‌بار',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، خیابان کهندژ، حوالی ترمینال باربری',
+    LAPTitle: 'اعلام بار پایانه شرق',
+    LAPIconName: 'x4',
+    LAPURL: ' ',
+    Description: ' ',
   },
   {
-    name: 'پایانه باربری وطن‌بار',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، خیابان امام خمینی، میدان کهندژ',
+    LAPTitle: 'اعلام بار سیمان سپاهان',
+    LAPIconName: 'x5',
+    LAPURL: ' ',
+    Description: ' ',
   },
   {
-    name: 'پایانه باربری ایران‌بار',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، جاده تهران، نرسیده به سه‌راهی خمینی‌شهر',
+    LAPTitle: 'اعلام بار نفت جی',
+    LAPIconName: 'x6',
+    LAPURL: ' ',
+    Description: ' ',
   },
-  {
-    name: 'پایانه باربری نقش جهان',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، میدان استقلال، روبروی ترمینال اتوبوسرانی',
-  },
-  {
-    name: 'پایانه باربری فولادشهر',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، ورودی فولادشهر، بلوار صنعت',
-  },
-  {
-    name: 'پایانه باربری خمینی‌شهر',
-    url: 'http://localhost:4200/',
-    description: 'اصفهان، جاده خمینی‌شهر، روبروی پلیس‌راه',
-  },
-  {
-    name: 'پایانه باربری شهرضا',
-    url: 'http://localhost:4200/',
-    description: 'شهرضا، جاده ترانزیت، ناحیه صنعتی شماره ۲',
-  },
-  {
-    name: 'پایانه باربری زرین‌شهر',
-    url: 'http://localhost:4200/',
-    description: 'زرین‌شهر، بلوار کارگر، مجاورت شهرک صنعتی',
-  },
-  {
-    name: 'پایانه باربری نجف‌آباد',
-    url: 'http://localhost:4200/',
-    description: 'نجف‌آباد، خیابان امام، ناحیه ترمینال باربری',
-  }
 ];
