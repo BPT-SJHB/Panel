7f72d8c38ca960f30e32ae3652789319bd2f154d
rezishon <rezashahmoradi.81@outlook.com>
2025-05-29 22:38:20 +0330

    feat(services/user-management)[user-management]: add `UserManagement` service
    This service will help us for management of users of system.

--- src/app/services/user-management/user-management.service.ts
+++ src/app/services/user-management/user-management.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { API_ROUTES } from 'app/constants/api';
+import { firstValueFrom } from 'rxjs';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { UserSession } from 'app/data/model/user-session.model';
+import { UserType } from 'app/data/model/user-type.model';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { handleHttpError } from 'app/utils/http-error-handler';
+import { SoftwareUserInfoResponse } from 'app/data/model/software-user-info-response.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserManagementService {
+export class UserManagementService implements OnInit {
+  constructor(private http: HttpClient, private userAuth: UserAuthService) {}
 
-  constructor() {console.log("hello") }
 }
