c35d5460e54b3c1d79e35e99d786d8dca3474f8e
rezishon <rezashahmoradi.81@outlook.com>
2025-05-29 22:32:51 +0330

    feat(data/model)[software-user-info-response]: add model for `SoftwareUserInfo` in response
    This model will be the respond object from API when we call the get
    information of user on it.

---
+++ src/app/data/model/software-user-info-response.model.ts
@@ -0,0 +1,8 @@
+export interface SoftwareUserInfoResponse {
+  UserId: number;
+  UserName: string;
+  MobileNumber: string;
+  UserTypeId: number;
+  UserActive: boolean;
+  SMSOwnerActive: boolean;
+}
