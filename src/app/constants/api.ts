7c10479613f45a33f7ea866555b4ff81b157bf28
rezishon <rezashahmoradi.81@outlook.com>
2025-05-29 22:30:44 +0330

    feat(constants)[api]: add route for new APIs
    Routes for `GetSoftwareUserInfo` & `GetUserTypes` &
    `RegisteringSoftwareUser` & `EditSoftwareUser` added.
    Also the variable structure changed. It all moved to `UserManagement`.

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -7,6 +7,12 @@ export const API_ROUTES = {
     CAPTCHA: `${environment.apiUrl}:${softwareUserAPIPort}/api/GetCaptcha`,
     Auth: `${environment.apiUrl}:${softwareUserAPIPort}/api/AuthUser`,
     Processes: `${environment.apiUrl}:${softwareUserAPIPort}/api/GetWebProcesses`,
+    UserManagement: {
+      GetSoftwareUserInfo: `${environment.apiUrl}:${softwareUserAPIPort}/api/GetSoftwareUser`,
+      GetUserTypes: `${environment.apiUrl}:${softwareUserAPIPort}/api/GetUserTypes`,
+      RegisteringSoftwareUser:`${environment.apiUrl}:${softwareUserAPIPort}/api/RegisteringSoftwareUser`,
+      EditSoftwareUser:`${environment.apiUrl}:${softwareUserAPIPort}/api/EditSoftwareUser`
+    },
   },
   TransportationAPI: {
     LoadAnnouncementPlaces: `${environment.apiUrl}:${transportationAPIPort}/api/GetLoadAnnouncementPlaces`,
