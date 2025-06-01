72f9578ea799b052aa81ffc01b0273063be9fd72
MohsenBg <moh.1380.1393@gmail.com>
2025-06-01 14:56:38 +0330

    chore(ngrx): setup NgRx store and integrate Redux DevTools
    - added StoreModule and DevToolsModule to AppModule
    - enabled time-travel debugging with Redux DevTools extension
    - configured initial store with tabs feature state

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,8 +12,10 @@ import { routes } from './app.routes';
 import { customTheme } from './themes/theme';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { sidebarReducer } from './store/sidebar/sidebar.reducer';
+import { tabReducer } from './store/tabs/tabs.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,7 +26,16 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({ theme: customTheme }),
     importProvidersFrom(ToastModule),
     provideStore({
-      sidebar: sidebarReducer
+      sidebar: sidebarReducer,
+      tabs:  tabReducer
+    }),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
     }),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
