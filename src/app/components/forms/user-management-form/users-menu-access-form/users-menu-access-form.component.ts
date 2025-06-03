b0156e92c1c599e0324e8ca950e652eba14dedd4
rezishon <rezashahmoradi.81@outlook.com>
2025-06-03 21:06:16 +0330

    chore(components/forms/user-management-form)[users-menu-access-form]: add headers
    Libs that we will need.

---
+++ src/app/components/forms/user-management-form/users-menu-access-form/users-menu-access-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TreeNode, TreeTableNode } from 'primeng/api';
+import { TreeTableModule } from 'primeng/treetable';
+import { CommonModule } from '@angular/common';
+import { ApiProcessesService } from 'app/services/api-processes/api-processes.service';
+import { UserManagementService } from 'app/services/user-management/user-management.service';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { ApiGroupProcess } from 'app/data/model/api-group-process.model';
+import { ApiProcess } from 'app/data/model/api-process.model';
+import { UserAuthService } from 'app/services/user-auth-service/user-auth.service';
+import { SearchInputComponent } from 'app/components/shared/inputs/search-input/search-input.component';
+import { filter } from 'rxjs';
+import { SoftwareUserInfo } from 'app/data/model/software-user-info.model';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ButtonModule } from 'primeng/button';
+@Component({
+  selector: 'app-users-menu-access-form',
+  imports: [
+    TreeTableModule,
+    CommonModule,
+    SearchInputComponent,
+    ButtonModule,
+    ProgressSpinnerModule,
+  ],
+  templateUrl: './users-menu-access-form.component.html',
+  styleUrl: './users-menu-access-form.component.scss',
+})
