2efed892d9373196c71a7506da6a1b3853ce0311
rezishon <rezashahmoradi.81@outlook.com>
2025-06-04 12:17:09 +0330

    feat(data/model)[truck-driver-info]: add `TruckDriverInfo` model
    This model will use of truck drivers information model.

---
+++ src/app/data/model/truck-driver-info.model.ts
@@ -0,0 +1,10 @@
+export interface TruckDriverInfo {
+  NationalCode?: string;
+  DriverId?: number;
+  NameFamily?: string;
+  MobileNumber?: string;
+  FatherName?: string;
+  DrivingLicenseNo?: string;
+  Address?: string;
+  SmartCardNo?: string;
+}
