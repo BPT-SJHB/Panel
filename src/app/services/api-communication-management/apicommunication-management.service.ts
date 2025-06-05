24380819fc366c5603998af037c0f18fa7d15102
rezishon <rezashahmoradi.81@outlook.com>
2025-06-05 23:31:11 +0330

    feat(services)[apicommunication-management]: update `CommunicateWithApi_Post()` to handle mocks
    Now the communication management service can handle system when API is
    disable and mocks should be use instead of real data.

--- src/app/services/api-communication-management/apicommunication-management.service.ts
+++ src/app/services/api-communication-management/apicommunication-management.service.ts
@@ -2,18 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ApiResponse } from 'app/data/model/api-Response.model';
 import { handleHttpError } from 'app/utils/http-error-handler';
+import { environment } from 'environments/environment';
 import { firstValueFrom } from 'rxjs';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { mockUserSession } from 'app/data/mock/user-session.mock';
 
 @Injectable({
   providedIn: 'root',
 })
 export class APICommunicationManagementService {
   private http = inject(HttpClient);
+  private userAuth = inject(UserAuthService);
 
   public async CommunicateWithAPI_Post<TBody, TExpect>(
     url: string,
-    bodyValue: TBody
+    bodyValue: TBody,
+    mockValue?: any
   ): Promise<ApiResponse<TExpect>> {
+    if (!environment.production && environment.disableApi) {
+      this.userAuth.setSessionId(mockUserSession.sessionId);
+      return {
+        success: true,
+        data: mockValue,
+      };
+    }
+
     const apiUrl = url;
 
     try {
@@ -25,7 +38,6 @@ export class APICommunicationManagementService {
 
       return { success: true, data: response };
     } catch (error: unknown) {
-      console.log(error)
       return handleHttpError<TExpect>(error);
     }
   }
