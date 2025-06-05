27b14af6d66ccb527a860550fcf0c58c8016d4dd
MohsenBg <moh.1380.1393@gmail.com>
2025-06-05 09:54:40 +0330

    feat(truck-driver-management-tab): add driver, truck info and wallet tabs to driver management tab

--- src/app/constants/tab-component-registry.ts
+++ src/app/constants/tab-component-registry.ts
@@ -1,13 +1,16 @@
 import { Type } from '@angular/core';
 import { MainTabComponent } from 'app/components/tabs/main-tab/main-tab.component';
 import { UserManagementTabComponent } from 'app/components/tabs/user-management-tab/user-management-tab.component';
+import { TruckDriverManagementTabComponent } from 'app/components/tabs/truck-driver-management-tab/truck-driver-management-tab.component';
 
 export enum TabComponentKey {
   Main = -1,
   UserManagement = 0,
+  TruckDriverManagement = 1,
 }
 
 export const TabComponentRegistry: Record<TabComponentKey, Type<any>> = {
   [TabComponentKey.Main]: MainTabComponent,
   [TabComponentKey.UserManagement]: UserManagementTabComponent,
+  [TabComponentKey.TruckDriverManagement]: TruckDriverManagementTabComponent, 
 };
