a9e219d5e6b4e31e2ab8f698570feddcc91f5a76
rezishon <rezashahmoradi.81@outlook.com>
2025-06-05 23:26:12 +0330

    feat(data/mock)[truck-driver-info]: add `mockTruckDriverInfo` mock

---
+++ src/app/data/mock/truck-driver-info.model.ts
@@ -0,0 +1,12 @@
+import { TruckDriverInfo } from '../model/truck-driver-info.model';
+
+export const mockTruckDriverInfo: TruckDriverInfo = {
+  DriverId: 1,
+  NameFamily: ';محمد;عباسي',
+  NationalCode: '5759871382',
+  MobileNumber: '09131210201',
+  FatherName: 'قياقلي',
+  DrivingLicenseNo: '4010300402',
+  Address: '',
+  SmartCardNo: '1228050',
+};
