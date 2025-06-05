49c1ccadd2ce6c5acda5eada0032f500562a94b2
rezishon <rezashahmoradi.81@outlook.com>
2025-06-05 23:27:21 +0330

    feat(data/mock)[user-types]: add `mockUserTypes` mock

---
+++ src/app/data/mock/user-types.mock.ts
@@ -0,0 +1,84 @@
+import { UserType } from '../model/user-type.model';
+
+export const mockUserTypes: UserType[] = [
+  {
+    UTId: 0,
+    UTTitle:
+      'نامعلوم                                                                                             ',
+  },
+  {
+    UTId: 1,
+    UTTitle:
+      'کاربر عادی                                                                                          ',
+  },
+  {
+    UTId: 2,
+    UTTitle:
+      'ادمین سیستم                                                                                         ',
+  },
+  {
+    UTId: 3,
+    UTTitle:
+      'راننده                                                                                              ',
+  },
+  {
+    UTId: 4,
+    UTTitle:
+      'مالک ناوگان باری                                                                                    ',
+  },
+  {
+    UTId: 5,
+    UTTitle:
+      'صنف کامیونداران                                                                                     ',
+  },
+  {
+    UTId: 6,
+    UTTitle:
+      'صنف شرکت های حمل و نقل                                                                              ',
+  },
+  {
+    UTId: 7,
+    UTTitle:
+      'شرکت حمل و نقل                                                                                      ',
+  },
+  {
+    UTId: 8,
+    UTTitle:
+      'انبار                                                                                               ',
+  },
+  {
+    UTId: 9,
+    UTTitle:
+      'ذوب آهن                                                                                             ',
+  },
+  {
+    UTId: 10,
+    UTTitle:
+      'شرکت خودگردان                                                                                       ',
+  },
+  {
+    UTId: 11,
+    UTTitle:
+      'مجتمع فولاد                                                                                         ',
+  },
+  {
+    UTId: 12,
+    UTTitle:
+      'معاونت حمل و نقل                                                                                    ',
+  },
+  {
+    UTId: 13,
+    UTTitle:
+      'مدیر پایانه                                                                                         ',
+  },
+  {
+    UTId: 14,
+    UTTitle:
+      'شرکت صدور بارنامه                                                                                   ',
+  },
+  {
+    UTId: 15,
+    UTTitle:
+      'کارخانجات و مراکز تولید بار                                                                         ',
+  },
+];
