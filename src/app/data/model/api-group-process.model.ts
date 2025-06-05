80a644ab7b16767d52d59ca6038ac56457a12081
rezishon <rezashahmoradi.81@outlook.com>
2025-06-05 23:28:52 +0330

    feat(data/model)[username-password]: update `ApiGroupProcess` model

--- src/app/data/model/api-group-process.model.ts
+++ src/app/data/model/api-group-process.model.ts
@@ -4,6 +4,6 @@ export interface ApiGroupProcess {
   PGId: number;
   PGTitle?: string;
   PGIconName?: string;
-  WebProcesses?: [ApiProcess];
+  WebProcesses: ApiProcess[];
   PGAccess?: boolean;
 }
