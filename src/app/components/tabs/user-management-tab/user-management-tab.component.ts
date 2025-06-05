fdd8e1beb4c15d38dfd82ffc223e4e3792fcc72b
MohsenBg <moh.1380.1393@gmail.com>
2025-06-05 09:44:15 +0330

    feat(user-access-menu, user-management-tab): run requests in parallel, add search bar, and integrate app-user-menu-access into user-management-tabs

--- src/app/components/tabs/user-management-tab/user-management-tab.component.ts
+++ src/app/components/tabs/user-management-tab/user-management-tab.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import {TabsModule} from "primeng/tabs";
 import { UserInfoFormComponent } from 'app/components/forms/user-Info-form/user-info-form.component';
+import { UsersMenuAccessFormComponent } from "../../forms/user-management-form/users-menu-access-form/users-menu-access-form.component";
 
 @Component({
   selector: 'app-user-management-tab',
-  imports: [TabsModule, UserInfoFormComponent],
+  imports: [TabsModule, UserInfoFormComponent, UsersMenuAccessFormComponent],
   templateUrl: './user-management-tab.component.html',
   styleUrl: './user-management-tab.component.scss'
 })
 export class UserManagementTabComponent {
-
-}
+  }
