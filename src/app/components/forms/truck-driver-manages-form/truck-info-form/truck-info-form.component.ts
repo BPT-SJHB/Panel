05fd89848c2e524718f53d628ffb72c12b79a2df
MohsenBg <moh.1380.1393@gmail.com>
2025-06-05 09:51:07 +0330

    feat(truck-info): add initial empty component for truck info form

---
+++ src/app/components/forms/truck-driver-manages-form/truck-info-form/truck-info-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-truck-info-form',
+  imports: [],
+  templateUrl: './truck-info-form.component.html',
+  styleUrl: './truck-info-form.component.scss'
+})
+export class TruckInfoFormComponent {
+}
