8595978425b8153b7bac3cf15ea9a271c3d2bc1f
rezishon <rezashahmoradi.81@outlook.com>
2025-06-07 12:27:25 +0330

    feat(data/mock)[fpc-info]: add `mockFPCsInfo` mock

---
+++ src/app/data/mock/fpc-info.mock.ts
@@ -0,0 +1,24 @@
+import { FPCInfo } from '../model/fpc-info.model';
+
+export const mockFPCsInfo: FPCInfo[] = [
+  {
+    FPCId: 1002,
+    FPCTitle: 'فولاد کوهپایه',
+    FPCTel: ' ',
+    FPCAddress: 'کوهپایه',
+    FPCManagerMobileNumber: '0912043215',
+    FPCManagerNameFamily: 'محمدی',
+    EmailAddress: 'y@gmail.com',
+    Active: true,
+  },
+  {
+    FPCId: 1003,
+    FPCTitle: 'فولاد کوير',
+    FPCTel: ' ',
+    FPCAddress: 'آران و بيدگل',
+    FPCManagerMobileNumber: '09234522155',
+    FPCManagerNameFamily: 'خوروش',
+    EmailAddress: 'z@gmail.com',
+    Active: true,
+  },
+];
