da2ba57196b6bdfa6dff9aa1adfa4e75894ca128
rezishon <rezashahmoradi.81@outlook.com>
2025-06-16 09:13:53 +0330

    feat(data/mock)(data/model)[loader-type]: add `LoaderTypes` model + mock data

---
+++ src/app/data/model/loader-type.model.ts
@@ -0,0 +1,8 @@
+export interface LoaderType {
+  LoaderTypeId: number;
+  LoaderTypeTitle: string;
+  LoaderTypeOrganizationId: number;
+  LoaderTypeFixStatusId: number;
+  LoaderTypeFixStatusTitle: string;
+  Active: boolean;
+}
