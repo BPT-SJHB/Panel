76d10896cec3a2e484010a351804a6a100287557
rezishon <rezashahmoradi.81@outlook.com>
2025-06-19 22:33:06 +0330

    fix(services)[captcha]: update methods to use `ApiCommunicationManagement` service

--- src/app/services/captcha-service/captcha.service.ts
+++ src/app/services/captcha-service/captcha.service.ts
@@ -1,51 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
 import { CaptchaChallenge } from 'app/data/model/captcha-challenge.model';
 import { ApiResponse } from 'app/data/model/api-Response.model';
 import { API_ROUTES } from 'app/constants/api';
-import { handleHttpError } from 'app/utils/http-error-handler';
-import { environment } from 'environments/environment';
 import { mockCaptcha } from 'app/data/mock/captcha-challenge.mock';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CaptchaService {
-  private readonly apiUrl = API_ROUTES.SoftwareUserAPI.CAPTCHA;
+  private apiCommunicator = inject(APICommunicationManagementService);
 
-  constructor(private http: HttpClient) {}
-
-  /**
-   * این تابع برای دریافت کپچا از سمت سرور است.
-   * کپچا شامل شناسه نشست و تصویر آن است.
-   */
   public async getCaptcha(): Promise<ApiResponse<CaptchaChallenge>> {
-    //#region  mock data
-    // این قسمت برای من هست که دسترسی به api ندارم
-    if (!environment.production && environment.disableApi) {
-      return {
-        success: true,
-        data: mockCaptcha,
-      };
-    }
-
+    //#region Consts
+    const apiUrl = API_ROUTES.SoftwareUserAPI.CAPTCHA;
     //#endregion
 
-    try {
-      const response = await firstValueFrom(
-        this.http.get<{ SessionId: string; Captcha: string }>(this.apiUrl)
-      );
+    //#region Request
+    const response = await this.apiCommunicator.CommunicateWithAPI_Get<{
+      SessionId: string;
+      Captcha: string;
+    }>(apiUrl, mockCaptcha);
+    //#endregion
 
-      return {
-        success: true,
-        data: {
-          sessionId: response.SessionId,
-          imageData: response.Captcha,
-        },
-      };
-    } catch (error: unknown) {
-      return handleHttpError<CaptchaChallenge>(error);
-    }
+    //#region Return
+    return {
+      success: response.success,
+      data: {
+        sessionId: response.data?.SessionId!,
+        imageData: response.data?.Captcha!,
+      },
+      error: response.error,
+    };
+    //#endregion
   }
 }
