95b98666387beba06dbaefc7b5401f24c447a355
rezishon <rezashahmoradi.81@outlook.com>
2025-06-21 12:34:08 +0330

    feat(services)[province-and-city-management]: add `ProvinceAndCitiesManagement` service

---
+++ src/app/services/province-city-management/province-and-city-management.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { API_ROUTES } from 'app/constants/api';
+import { mockProvinceAndCities } from 'app/data/mock/province-city.mock';
+import { City, Province } from 'app/data/model/province-city.model';
+import { mockShortResponse } from 'app/data/mock/short-response.mock';
+import { ShortResponse } from 'app/data/model/short-response.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProvinceAndCityManagementService {
+}
