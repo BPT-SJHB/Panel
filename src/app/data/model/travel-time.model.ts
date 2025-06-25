b13973df5f3002408220308a609c1ad65aa7319f
rezishon <rezashahmoradi.81@outlook.com>
2025-06-25 14:15:52 +0330

    feat(data/model)[travel-time]: add `travel-time` model

---
+++ src/app/data/model/travel-time.model.ts
@@ -0,0 +1,9 @@
+export interface TravelTime {
+  LoaderTypeId?: number;
+  SourceCityId?: number;
+  SourceCityName?: string;
+  TargetCityId?: number;
+  TargetCityName?: string;
+  TravelTime?: number;
+  Active?: boolean;
+}
