5aa777f0e64c9b9b1a58af77bd0229a92ad55528
rezishon <rezashahmoradi.81@outlook.com>
2025-06-26 22:58:19 +0330

    feat(services)[travel-time-management]: add `EditTravelTime` method
    It will edit the TravelTime value in the `TravelTime` model.(the number)
    `isLoggedIn()` added.
    Send a post request with `TravelTime`model to the API and will return
    `ShortResponse`.

--- src/app/services/travel-time-management/travel-time-management.service.ts
+++ src/app/services/travel-time-management/travel-time-management.service.ts
@@ -120,4 +120,34 @@ export class TravelTimeManagementService {
     >(apiUrl, bodyValue, mockShortResponse);
     //#endregion
   }
+
+  public async EditTravelTime(
+    loaderTypeId: number,
+    sourceCityId: number,
+    targetCityId: number,
+    time: number
+  ): Promise<ApiResponse<ShortResponse>> {
+    this.userAuth.isLoggedIn();
+
+    //#region Consts
+    const apiUrl = API_ROUTES.TransportationAPI.TravelTime.EditTravelTime;
+    const travelTimeInfo: TravelTime = {
+      LoaderTypeId: loaderTypeId,
+      SourceCityId: sourceCityId,
+      TargetCityId: targetCityId,
+      TravelTime: time,
+    };
+    const bodyValue = {
+      SessionId: this.userAuth.getSessionId(),
+      TravelTime: travelTimeInfo,
+    };
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      ShortResponse
+    >(apiUrl, bodyValue, mockShortResponse);
+    //#endregion
+  }
 }
