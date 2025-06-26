7a76abc68ba3557a3221a3767c106b2f10cbaf5c
rezishon <rezashahmoradi.81@outlook.com>
2025-06-26 22:39:19 +0330

    fix(services)[lad-place-management]: update mock data

--- src/app/services/lad-place-management/lad-place-management.service.ts
+++ src/app/services/lad-place-management/lad-place-management.service.ts
@@ -78,7 +78,7 @@ export class LADPlaceManagementService {
     const response = await this.apiCommunicator.CommunicateWithAPI_Post<
       typeof bodyValue,
       LADPlace
-    >(apiUrl, bodyValue, mockShortResponse);
+    >(apiUrl, bodyValue, mockLADPlaces[0].LADPlaceId);
     //#endregion
 
     //#region Return
