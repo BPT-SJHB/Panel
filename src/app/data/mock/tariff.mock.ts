8e26b4883d0a8c07d5c0b6e4d1405981912b15c6
rezishon <rezashahmoradi.81@outlook.com>
2025-06-27 18:19:33 +0330

    feat(data/mock+data)[tariff]: add `tariff` model + mock data

---
+++ src/app/data/mock/tariff.mock.ts
@@ -0,0 +1,49 @@
+import { Tariff } from '../model/tariff.model';
+
+export const mockTariffs: Tariff[] = [
+  {
+    SourceCityId: 21451004,
+    SourceCityTitle: 'اصفهان - ذوب آهن',
+    TargetCityId: 11320000,
+    TargetCityTitle: 'تهران',
+    LoaderTypeId: 504,
+    LoaderTypeTitle:
+      'كفي 12چرخ\r\n                                                                                         ',
+    GoodId: 2710000,
+    GoodTitle: 'آهن آلات',
+    Tariff: 13020000,
+    BaseTonnag: 22,
+    CalculationReference: '',
+    Active: true,
+  },
+  {
+    SourceCityId: 21451004,
+    SourceCityTitle: 'اصفهان - ذوب آهن',
+    TargetCityId: 11320000,
+    TargetCityTitle: 'تهران',
+    LoaderTypeId: 504,
+    LoaderTypeTitle:
+      'كفي 12چرخ\r\n                                                                                         ',
+    GoodId: 2710000,
+    GoodTitle: 'آهن آلات',
+    Tariff: 15950000,
+    BaseTonnag: 22,
+    CalculationReference: '',
+    Active: true,
+  },
+  {
+    SourceCityId: 21451004,
+    SourceCityTitle: 'اصفهان - ذوب آهن',
+    TargetCityId: 11320000,
+    TargetCityTitle: 'تهران',
+    LoaderTypeId: 504,
+    LoaderTypeTitle:
+      'كفي 12چرخ\r\n                                                                                         ',
+    GoodId: 2710000,
+    GoodTitle: 'آهن آلات',
+    Tariff: 17447000,
+    BaseTonnag: 22,
+    CalculationReference: '',
+    Active: true,
+  },
+];
