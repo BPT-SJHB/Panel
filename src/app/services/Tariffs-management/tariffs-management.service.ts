70045841431426bd0b6b31da66e3e4bbae73fb95
rezishon <rezashahmoradi.81@outlook.com>
2025-07-03 11:37:46 +0330

    feat(services)[tariffs-management]: add `GetTariffs` method
    The method will get 4 parameters which will map to the `Tariff` model.
    It will return array of `Tariff`.
    After it checks the user session validation, It will handle the body of
    our request.
    The first parameter should be present. One of three others should be at
    least have valid value. It will check every three parameter, and if it
    goes okey, it will added to the body of our POST request. At the end of
    checking, it will sent a default value.

--- src/app/services/Tariffs-management/tariffs-management.service.ts
+++ src/app/services/Tariffs-management/tariffs-management.service.ts
@@ -14,4 +14,58 @@ import { mockShortResponse } from 'app/data/mock/short-response.mock';
 export class TariffsManagementService {
   private userAuth = inject(UserAuthService);
   private apiCommunicator = inject(APICommunicationManagementService);
+
+  public async GetTariffs(
+    loaderTypeId: number,
+    sourceCityId: number | undefined,
+    targetCityId: number | undefined,
+    productId: number | undefined
+  ): Promise<ApiResponse<Tariff[]>> {
+    this.userAuth.isLoggedIn();
+
+    //#region Consts
+    const apiUrl = API_ROUTES.TransportationAPI.Tariffs.GetTariffs;
+    const tariffsInfo: Tariff = {
+      LoaderTypeId: loaderTypeId,
+      SourceCityId: sourceCityId ?? undefined,
+      TargetCityId: targetCityId ?? undefined,
+      GoodId: productId ?? undefined,
+    };
+    const bodyValue: {
+      SessionId: string;
+      LoaderTypeId: number;
+      SourceCityId?: number;
+      TargetCityId?: number;
+      GoodId?: number;
+    } = {
+      SessionId: this.userAuth.getSessionId() ?? '',
+      LoaderTypeId: tariffsInfo.LoaderTypeId,
+    };
+
+    if (tariffsInfo.SourceCityId != undefined) {
+      bodyValue.SourceCityId = tariffsInfo.SourceCityId;
+    }
+    if (tariffsInfo.TargetCityId != undefined) {
+      bodyValue.TargetCityId = tariffsInfo.TargetCityId;
+    }
+    if (tariffsInfo.GoodId != undefined) {
+      bodyValue.GoodId = tariffsInfo.GoodId;
+    }
+    if (
+      tariffsInfo.SourceCityId != undefined &&
+      tariffsInfo.TargetCityId != undefined &&
+      tariffsInfo.GoodId != undefined
+    ) {
+      bodyValue.SourceCityId = 0;
+    }
+
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      Tariff[]
+    >(apiUrl, bodyValue, mockTariffs);
+    //#endregion
+  }
 }
