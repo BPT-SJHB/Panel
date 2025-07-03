1721e0b78f3f5b585074a96f87f00becd76dabe4
rezishon <rezashahmoradi.81@outlook.com>
2025-07-03 11:33:17 +0330

    feat(services)[tariffs-management]: add `TariffsManagement` service

---
+++ src/app/services/Tariffs-management/tariffs-management.service.ts
@@ -0,0 +1,15 @@
+import { inject, Injectable } from '@angular/core';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { API_ROUTES } from 'app/constants/api';
+import { Tariff } from 'app/data/model/tariff.model';
+import { mockTariffs } from 'app/data/mock/tariff.mock';
+import { ShortResponse } from 'app/data/model/short-response.model';
+import { mockShortResponse } from 'app/data/mock/short-response.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TariffsManagementService {
+}
