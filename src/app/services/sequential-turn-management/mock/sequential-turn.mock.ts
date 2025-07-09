75221cf90b6dccc55a2887d41f8ef710982dbe42
rezishon <rezashahmoradi.81@outlook.com>
2025-07-09 22:58:10 +0330

    feat(services/mock)[sequential-turn]: add `mockSequentialTurns` mock

---
+++ src/app/services/sequential-turn-management/mock/sequential-turn.mock.ts
@@ -0,0 +1,32 @@
+import { SequentialTurn } from '../model/sequential-turn.model';
+
+export const mockSequentialTurns: SequentialTurn[] = [
+  {
+    SeqTurnId: 2,
+    SeqTurnTitle:
+      'تریلی برون شهری - آهن آلات انباری و ذوبی ، رول سبا                                                  ',
+    SeqTurnKeyWord: 'T',
+    Active: true,
+  },
+  {
+    SeqTurnId: 3,
+    SeqTurnTitle:
+      'تریلی شهری - آهن آلات انباری و ذوبی ، رول سبا                                                       ',
+    SeqTurnKeyWord: 'S',
+    Active: true,
+  },
+  {
+    SeqTurnId: 5,
+    SeqTurnTitle:
+      'تریلی  برون شهری - آهن آلات ذوبی صادراتی                                                            ',
+    SeqTurnKeyWord: 'X',
+    Active: true,
+  },
+  {
+    SeqTurnId: 6,
+    SeqTurnTitle:
+      'تریلی - بارهای رسوبی                                                                                ',
+    SeqTurnKeyWord: 'R',
+    Active: true,
+  },
+];
