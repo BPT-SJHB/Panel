93a90a99d14c710944ca879ccff72d49ad959266
rezishon <rezashahmoradi.81@outlook.com>
2025-07-09 09:18:58 +0330

    fix(data/model)[travel-time]: add `LoaderTypeTitle` to `TravelTime` model

--- src/app/data/model/travel-time.model.ts
+++ src/app/data/model/travel-time.model.ts
@@ -2,6 +2,7 @@ export interface TravelTime {
   LoaderTypeId: number;
   SourceCityId: number | undefined;
   TargetCityId: number | undefined;
+  LoaderTypeTitle?: string;
   SourceCityName?: string;
   TargetCityName?: string;
   TravelTime?: number;
