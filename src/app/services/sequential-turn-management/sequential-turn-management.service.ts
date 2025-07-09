c5a1b5ac02dfbdca978c219bc9b3fed8a7ac60af
rezishon <rezashahmoradi.81@outlook.com>
2025-07-09 23:03:46 +0330

    feat(services)[sequential-turn-management]: add `SequentialTurnManagement` service

---
+++ src/app/services/sequential-turn-management/sequential-turn-management.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import { SequentialTurn } from './model/sequential-turn.model';
+import { API_ROUTES } from 'app/constants/api';
+import { mockSequentialTurns } from './mock/sequential-turn.mock';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { ShortResponse } from 'app/data/model/short-response.model';
+import { mockShortResponse } from 'app/data/mock/short-response.mock';
+import { RelationOfSequentialTurnToLoaderType } from './model/relation-of-sequentialTurn-to-loaderType.model';
+import { mockRelationOfSequentialTurnToLoaderTypes } from './mock/relation-of-sequentialTurn-to-loaderType.mock';
+import { RelationOfSequentialTurnToAnnouncementSubGroup } from './model/relation-of-sequentialTurn-to-announcementSubGroup.model';
+import { mockRelationOfSequentialTurnToAnnouncementSubGroups } from './mock/relation-of-sequentialTurn-to-announcementSubGroup.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SequentialTurnManagementService {
+}
