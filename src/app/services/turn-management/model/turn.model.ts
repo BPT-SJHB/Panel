8a4696c4514a4e49c48d4996bb00ace61596b8d4
rezishon <rezashahmoradi.81@outlook.com>
2025-07-13 12:14:03 +0330

    feat(services/turn-management)[turn.model]: add `turn` model

---
+++ src/app/services/turn-management/model/turn.model.ts
@@ -0,0 +1,13 @@
+export interface TurnInfo {
+  TurnId: number;
+  TurnIssueDate?: string;
+  TurnIssueTime?: string;
+  TruckDriver?: string;
+  SoftwareUserName?: string;
+  BillOfLadingNumber?: string;
+  OtaghdarTurnNumber?: string;
+  TurnStatusTitle?: string;
+  TurnStatusDescription?: string;
+  DateOfLastChanged?: string;
+  SequentialTurnTitle?: string;
+}
