84fca4d42b18d9c44e98ad9394aa17c5cad72e16
rezishon <rezashahmoradi.81@outlook.com>
2025-07-13 12:12:49 +0330

    feat(services/turn-management)[turn.mock]: add `turn` mock

---
+++ src/app/services/turn-management/mock/turn.model.ts
@@ -0,0 +1,130 @@
+import { TurnInfo } from '../model/turn.model';
+
+export const mockTurnInfos: TurnInfo[] = [
+  {
+    TurnId: 7,
+    TurnIssueDate: '1404/04/09',
+    TurnIssueTime: '22:04:39       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '',
+    OtaghdarTurnNumber:
+      'Z1404/000003:7                                                                                      ',
+    TurnStatusTitle:
+      'فعال                                                                                                ',
+    TurnStatusDescription:
+      'صادر شده                                                                                            ',
+    DateOfLastChanged: '',
+    SequentialTurnTitle:
+      'مرتضي شاهمرادي                                                                                      ',
+  },
+  {
+    TurnId: 6,
+    TurnIssueDate: '1404/04/09',
+    TurnIssueTime: '22:04:37       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '',
+    OtaghdarTurnNumber:
+      'Z1404/000003:6                                                                                      ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'مرتضي شاهمرادي                                                                                      ',
+  },
+  {
+    TurnId: 5,
+    TurnIssueDate: '1404/04/09',
+    TurnIssueTime: '21:25:24       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '',
+    OtaghdarTurnNumber:
+      'Z1404/000003                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'مرتضي شاهمرادي                                                                                      ',
+  },
+  {
+    TurnId: 4,
+    TurnIssueDate: '1404/04/09',
+    TurnIssueTime: '21:13:41       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '',
+    OtaghdarTurnNumber:
+      'Z1404/000002                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'مرتضي شاهمرادي                                                                                      ',
+  },
+  {
+    TurnId: 3,
+    TurnIssueDate: '1404/04/09',
+    TurnIssueTime: '21:01:17       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '',
+    OtaghdarTurnNumber:
+      'B1404/000001                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'شهري - شمش کوهپايه                                                                                  ',
+  },
+  {
+    TurnId: 2,
+    TurnIssueDate: '1404/04/05',
+    TurnIssueTime: '00:00:00       ',
+    TruckDriver: 'حسن عباسي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '1404/1/12',
+    OtaghdarTurnNumber:
+      'T1404/00002                                                                                         ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+  },
+  {
+    TurnId: 1,
+    TurnIssueDate: '1404/03/11',
+    TurnIssueTime: '00:00:00       ',
+    TruckDriver: 'حسن عباسي',
+    SoftwareUserName:
+      'مرتضي شاهمرادي                                                                                      ',
+    BillOfLadingNumber: '1404/1/12',
+    OtaghdarTurnNumber:
+      'T1404/00001                                                                                         ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    TurnStatusDescription:
+      'باطل شده - کاربر                                                                                    ',
+    DateOfLastChanged: '1404/04/09',
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+  },
+];
