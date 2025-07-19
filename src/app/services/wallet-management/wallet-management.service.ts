86fd35de20790df3454da3a0da00e95aacdab112
rezishon <rezashahmoradi.81@outlook.com>
2025-07-19 13:43:51 +0330

    feat(services)[wallet-management]: add `WalletManagement` service

---
+++ src/app/services/wallet-management/wallet-management.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { WalletTransaction } from './model/wallet-transaction.model';
+import { API_ROUTES } from 'app/constants/api';
+import { Wallet } from './model/wallet.model';
+import { mockWalletTransactions } from './mock/wallet-transaction.mock';
+import { WalletPaymentHistory } from './model/wallet-payment-history.model';
+import { mockWalletPaymentHistories } from './mock/wallet-payment-history.mock';
+import { mockWallet } from './mock/wallet.mock';
+@Injectable({
+  providedIn: 'root',
+})
+export class WalletManagementService {
+}
