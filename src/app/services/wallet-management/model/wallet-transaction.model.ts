5474b24b43901397cb111a8c00601a1bd37c22f0
rezishon <rezashahmoradi.81@outlook.com>
2025-07-19 13:03:07 +0330

    feat(services/wallet-management/model)[wallet-transaction]: add `WalletTransaction` model

---
+++ src/app/services/wallet-management/model/wallet-transaction.model.ts
@@ -0,0 +1,10 @@
+export interface WalletTransaction {
+  TransactionTitle: string;
+  TransactionColor: string;
+  ShamsiDate: string;
+  Time: string;
+  CurrentBalance: number;
+  Amount: number;
+  Reminder: number;
+  UserName: string;
+}
