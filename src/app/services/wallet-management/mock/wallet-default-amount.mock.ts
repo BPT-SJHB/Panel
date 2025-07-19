aa9a60d68deebfe67c894ad23499f6a1d3113f6b
rezishon <rezashahmoradi.81@outlook.com>
2025-07-19 14:43:28 +0330

    feat(services/wallet-management/mock)[wallet-default-amount]: add `mockWalletDefaultAmounts` mock

---
+++ src/app/services/wallet-management/mock/wallet-default-amount.mock.ts
@@ -0,0 +1,28 @@
+import { WalletDefaultAmount } from '../model/wallet-default-amount.model';
+
+export const mockWalletDefaultAmounts: WalletDefaultAmount[] = [
+  {
+    AmountTitle: '10000',
+    Amount: 10000,
+  },
+  {
+    AmountTitle: '40000',
+    Amount: 40000,
+  },
+  {
+    AmountTitle: '50000',
+    Amount: 50000,
+  },
+  {
+    AmountTitle: '100000',
+    Amount: 100000,
+  },
+  {
+    AmountTitle: '200000',
+    Amount: 200000,
+  },
+  {
+    AmountTitle: '300000',
+    Amount: 300000,
+  },
+];
