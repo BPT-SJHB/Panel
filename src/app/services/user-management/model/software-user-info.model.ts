7a293103da7d2d973bed78937f327227d4ee27c1
rezishon <rezashahmoradi.81@outlook.com>
2025-07-20 14:39:00 +0330

    fix(services/mock + services/model)[software-user-info]: replace `SoftwareUserInfo` model and `mockSoftwareUserInfo` mock

---
+++ src/app/services/user-management/model/software-user-info.model.ts
@@ -0,0 +1,9 @@
+export interface SoftwareUserInfo {
+  UserId: number;
+  UserName?: string;
+  MobileNumber?: string;
+  UserTypeId?: number;
+  UserActive?: boolean;
+  SMSOwnerActive?: boolean;
+  UserTypeTitle?: string;
+}
