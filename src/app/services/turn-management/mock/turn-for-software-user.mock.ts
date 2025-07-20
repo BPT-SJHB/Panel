11dc98546562fbe8a397cebfe5f2555cf5992ea0
rezishon <rezashahmoradi.81@outlook.com>
2025-07-20 14:35:17 +0330

    feat(services/mock)[turn-for-software-user]: add `mockTurnsForSoftwareUser` mock

---
+++ src/app/services/turn-management/mock/turn-for-software-user.mock.ts
@@ -0,0 +1,64 @@
+import { TurnForSoftwareUser } from '../model/turn-for-software-user.model';
+
+export const mockTurnsForSoftwareUser: TurnForSoftwareUser[] = [
+  {
+    TurnDistanceToValidity: 0,
+    TurnId: 16,
+    TurnIssueDate: '1404/04/25',
+    TurnIssueTime: '13:54:53       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SequentialTurn:
+      'T1404/000005                                                                                        ',
+    TurnStatusTitle:
+      'فعال                                                                                                ',
+    LPString: '286ع11-13',
+  },
+  {
+    TurnDistanceToValidity: 0,
+    TurnId: 15,
+    TurnIssueDate: '1404/04/25',
+    TurnIssueTime: '10:46:35       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SequentialTurn:
+      'T1404/000004                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    LPString: '286ع11-13',
+  },
+  {
+    TurnDistanceToValidity: 0,
+    TurnId: 14,
+    TurnIssueDate: '1404/04/25',
+    TurnIssueTime: '09:55:18       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SequentialTurn:
+      'T1404/000003                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    LPString: '286ع11-13',
+  },
+  {
+    TurnDistanceToValidity: 0,
+    TurnId: 13,
+    TurnIssueDate: '1404/04/22',
+    TurnIssueTime: '21:19:53       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SequentialTurn:
+      'Z1404/000006                                                                                        ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    LPString: '286ع11-13',
+  },
+  {
+    TurnDistanceToValidity: 0,
+    TurnId: 11,
+    TurnIssueDate: '1404/04/22',
+    TurnIssueTime: '13:08:46       ',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    SequentialTurn:
+      'Z1404/000005:11                                                                                     ',
+    TurnStatusTitle:
+      'غیرفعال                                                                                             ',
+    LPString: '286ع11-13',
+  },
+];
