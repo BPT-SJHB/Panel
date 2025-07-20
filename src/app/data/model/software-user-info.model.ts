707bccdf486981d4a8cc56eece3bec203c5b07a4
rezishon <rezashahmoradi.81@outlook.com>
2025-07-20 14:29:55 +0330

    feat(components/forms + data/mock + data/model + services)[user-info-form + users-menu-access-form + software-user-info + user-auth]: update import API Routes

--- src/app/data/model/software-user-info.model.ts
+++
@@ -1,9 +0,0 @@
-export interface SoftwareUserInfo {
-  UserId: number;
-  UserName?: string;
-  MobileNumber?: string;
-  UserTypeId?: number;
-  UserActive?: boolean;
-  SMSOwnerActive?: boolean;
-  UserTypeTitle?: string;
-}
