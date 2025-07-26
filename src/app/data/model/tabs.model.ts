721e4af78633e620d314f2df23e35f038a8f139e
MohsenBg <moh.1380.1393@gmail.com>
2025-07-26 20:06:11 +0330

    feat(tabs): add DynamicTab interface for managing tab state and lifecycle

--- src/app/data/model/tabs.model.ts
+++ src/app/data/model/tabs.model.ts
@@ -1,9 +1,27 @@
-import { TabComponentKey } from "app/constants/tab-component-registry";
+import { ComponentRef, WritableSignal } from '@angular/core';
+import {
+  TabComponentKey,
+  TabConfig,
+} from 'app/constants/tab-component-registry';
 
 export interface TabItem {
-    id:number,
-    title:string,
-    closable:boolean
-    component:TabComponentKey,  
-    active:boolean
+  id: number;
+  title: string;
+  closable: boolean;
+  component: TabComponentKey;
+  active: boolean;
+}
+
+/** ================================
+ *  Interfaces
+ *  ================================ */
+export interface DynamicTab {
+  id: string;
+  title: string;
+  icon: string;
+  closeable: boolean;
+  sharedSignal?: WritableSignal<any>;
+  cachedComponent: Map<number, ComponentRef<any>>;
+  selectedSubTab: number;
+  config: TabConfig;
 }
