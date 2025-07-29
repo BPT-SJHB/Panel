c5114ee575ac9692feab4ca30ba742a93198e35e
MohsenBg <moh.1380.1393@gmail.com>
2025-07-29 10:01:25 +0330

    feat(tab): make tab ID optional to reuse existing tabs instead of creating duplicates

--- src/app/store/tab/tab.actions.ts
+++ src/app/store/tab/tab.actions.ts
@@ -1,11 +1,5 @@
 import { createAction, props } from "@ngrx/store";
-import { TabComponentKey } from "app/constants/tab-component-registry";
 import { TabData } from "./tab.reducer";
 
-export const addTab = createAction("[Tabs] Create", props<{
-    title: string,
-    closable: boolean,
-    component: TabComponentKey, 
-}>());
 
 export const createTab = createAction("[Tab] Create", props<TabData>());
--- src/app/store/tab/tab.reducer.ts
+++ src/app/store/tab/tab.reducer.ts
@@ -1,19 +1,19 @@
 // tab.reducer.ts
 import { createReducer, on } from '@ngrx/store';
-import {
-  TabComponentKey,
-  TabComponentRegistry,
-} from 'app/constants/tab-component-registry';
+import { TabComponentKey } from 'app/constants/tab-component-registry';
 import { createTab } from './tab.actions';
 
 export interface TabData {
+  id?: string;
   title: string;
   icon: string;
   component: TabComponentKey;
   closeable: boolean;
 }
 
+export const DEFAULT_MAIN_TAB_ID = '00000000-0000-0000-0000-000000000000';
 const initialState: TabData = {
+  id: DEFAULT_MAIN_TAB_ID,
   title: 'صفحه اصلی',
   icon: 'pi pi-class',
   component: TabComponentKey.Main,
@@ -22,7 +22,7 @@ const initialState: TabData = {
 
 export const tabReducer = createReducer(
   initialState,
-  on(createTab, (state, { title, icon, component, closeable }) => {
-    return { ...state, title, icon, component, closeable };
+  on(createTab, (state, { id, title, icon, component, closeable }) => {
+    return { ...state, id, title, icon, component, closeable };
   })
 );
