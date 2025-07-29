fdbf13bad639a5b25072cfc307d48efd7f95a56f
MohsenBg <moh.1380.1393@gmail.com>
2025-07-29 09:50:09 +0330

    refactor(header): move header file to /header/header and add  user-profile and wallet-profile components

---
+++ src/app/components/shared/layout/header/header/header.component.ts
@@ -0,0 +1,69 @@
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { HeaderData } from 'app/data/model/header-data.model';
+import { selectContent } from 'app/store/content-manager/content-manager.selectors';
+import { openSidebar } from 'app/store/sidebar/sidebar.actions';
+import { ButtonModule } from 'primeng/button';
+import { Subscription } from 'rxjs';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { ToastService } from 'app/services/toast-service/toast.service';
+import { UserManagementService } from 'app/services/user-management/user-management.service';
+import { checkAndToastError } from 'app/utils/api-utils';
+import { RawSoftwareUserForProfile } from 'app/services/user-management/model/software-user-profile.model';
+import { Wallet } from 'app/services/wallet-management/model/wallet.model';
+import { WalletProfileComponent } from "../wallet-profile/wallet-profile.component";
+@Component({
+  selector: 'app-header',
+  imports: [ButtonModule, UserProfileComponent, WalletProfileComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
+  private store = inject(Store);
+  private render$?: Subscription;
+  private toast = inject(ToastService);
+  private userService = inject(UserManagementService);
+
+  readonly profile = signal<RawSoftwareUserForProfile | null>(null);
+  readonly wallet = signal<Wallet | null>(null);
+
+  headerData: HeaderData = {
+    title: 'عنوان صفحه',
+    icon: 'pi-user',
+  };
+
+  ngAfterViewInit(): void {
+    this.render$ = this.store.select(selectContent).subscribe((render) => {
+      this.headerData = {
+        title: render.title,
+        icon: render.icon,
+      };
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.render$?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.loadUserProfile();
+  }
+
+  openSidebar() {
+    this.store.dispatch(openSidebar());
+  }
+
+  private async loadUserProfile() {
+    const response = await this.userService.GetSoftwareUserProfile();
+    if (!checkAndToastError(response, this.toast)) return;
+    this.profile.set(response.data.RawSoftwareUser);
+    this.wallet.set(response.data.MoneyWallet);
+  }
+}
