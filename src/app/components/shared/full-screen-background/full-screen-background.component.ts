6ce524d151f3a1595d284363f0fde5e58b346a10
rezishon <rezashahmoradi.81@outlook.com>
2025-08-06 09:53:33 +0330

    refactor(components/forms + components/shared)[login-form + form-card + full-screen-background]: update login page style, background, and code

--- src/app/components/shared/full-screen-background/full-screen-background.component.ts
+++ src/app/components/shared/full-screen-background/full-screen-background.component.ts
@@ -5,11 +5,14 @@ import { Component, Input } from '@angular/core';
   selector: 'app-full-screen-background',
   imports: [NgStyle],
   templateUrl: './full-screen-background.component.html',
-  styleUrl: './full-screen-background.component.scss'
+  styleUrl: './full-screen-background.component.scss',
 })
 export class FullScreenBackgroundComponent {
-  @Input() imageUrl: string = 'background_form_login.webp';
-  @Input() backgroundSize:string = "cover";
-  @Input() backgroundPosition :string = "center";
+  @Input() lgImageUrl: string = 'login_form_lg_background.webp';
+  @Input() lgBackgroundSize: string = 'cover';
+  @Input() lgBackgroundPosition: string = 'right';
+  @Input() imageUrl: string = 'login_form_background.webp';
+  @Input() backgroundSize: string = 'cover';
+  @Input() backgroundPosition: string = 'left';
   @Input() blur: number = 0;
 }
