c6875ecce9fac03d64e8c85f1b08688e6a93ef19
rezishon <rezashahmoradi.81@outlook.com>
2025-08-07 13:49:52 +0330

    feat(app/pages/home)[map-svg]: add iran map SVG
    The Iran map svg added and also it's responsive. The color is based on
    the primary color of theme. It can found the clicked province.

---
+++ src/app/pages/home/map-svg/map-svg.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-svg',
+  imports: [],
+  templateUrl: './map-svg.component.html',
+  styleUrl: './map-svg.component.scss',
+})
+export class MapSvgComponent {
+  provinceNames: { [key: string]: string } = {
+    'IR-01': 'آذربایجان شرقی',
+    'IR-02': 'آذربایجان غربی',
+    'IR-03': 'اردبیل',
+    'IR-04': 'اصفهان',
+    'IR-05': 'ایلام',
+    'IR-06': 'بوشهر',
+    'IR-07': 'تهران',
+    'IR-08': 'چهارمحال و بختیاری',
+    'IR-10': 'خوزستان',
+    'IR-11': 'زنجان',
+    'IR-12': 'سمنان',
+    'IR-13': 'سیستان و بلوچستان',
+    'IR-14': 'فارس',
+    'IR-15': 'کرمان',
+    'IR-16': 'کردستان',
+    'IR-17': 'کرمانشاه',
+    'IR-18': 'کهگیلویه و بویراحمد',
+    'IR-19': 'گیلان',
+    'IR-20': 'لرستان',
+    'IR-21': 'مازندران',
+    'IR-22': 'مرکزی',
+    'IR-23': 'هرمزگان',
+    'IR-24': 'همدان',
+    'IR-25': 'یزد',
+    'IR-26': 'قم',
+    'IR-27': 'گلستان',
+    'IR-28': 'قزوین',
+    'IR-29': 'خراسان جنوبی',
+    'IR-30': 'خراسان رضوی',
+    'IR-31': 'خراسان شمالی',
+    'IR-32': 'البرز',
+  };
+
+  selectedProvinceId: string | null = null;
+
+  onProvinceClick(provinceId: string): void {
+    this.selectedProvinceId = provinceId;
+    const provinceName = this.provinceNames[provinceId] || 'Unknown';
+
+    console.log(`Province with ID '${provinceId}' clicked.`);
+    console.log(`Province name: ${provinceName}`);
+  }
+}
