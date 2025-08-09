0c58c7252cefdf553a46269c48d36bf26503976a
MohsenBg <moh.1380.1393@gmail.com>
2025-08-09 06:56:51 +0330

    refactor(tableUi):add stripedRow to table config

--- src/app/constants/ui/table.ui.ts
+++ src/app/constants/ui/table.ui.ts
@@ -2,6 +2,7 @@ export const TableConfig = {
   paginator: true,
   rows: 10,
   rowsPerPageOptions: [5, 10, 20, 50, 100],
-  paginatorLocale:'fa',
+  paginatorLocale: 'fa',
+  stripedRows: true,
   tableStyle: { 'min-width': '50rem' },
 };
