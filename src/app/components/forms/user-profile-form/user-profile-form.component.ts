e3516794bddccfcb17216c5252fc649a5fa8eff1
MohsenBg <moh.1380.1393@gmail.com>
2025-08-09 06:51:14 +0330

    feat(user-profile):add exit button on mobile screen size

--- src/app/components/forms/user-profile-form/user-profile-form.component.ts
+++ src/app/components/forms/user-profile-form/user-profile-form.component.ts
@@ -2,7 +2,6 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 
-
 // 🔽 PrimeNG UI Modules
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
@@ -16,7 +15,8 @@ import { checkAndToastError } from 'app/utils/api-utils';
 import { NewPasswordDialogComponent } from 'app/components/shared/dialog/new-password-dialog/new-password-dialog.component';
 import { ConfirmationService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ButtonComponent } from "app/components/shared/button/button.component";
+import { ButtonComponent } from 'app/components/shared/button/button.component';
+import { UserAuthService } from 'app/services/user-auth-service/user-auth.service';
 
 // 🔽 Interfaces
 interface UserProfile {
@@ -35,7 +35,7 @@ interface UserProfile {
   selector: 'app-user-profile-form',
   standalone: true,
   imports: [AvatarModule, ButtonModule, ConfirmDialogModule, ButtonComponent],
-  providers: [ConfirmationService,DialogService],
+  providers: [ConfirmationService, DialogService],
   templateUrl: './user-profile-form.component.html',
   styleUrl: './user-profile-form.component.scss',
 })
@@ -46,6 +46,7 @@ export class UserProfileFormComponent implements OnInit {
   private readonly loadingService = inject(LoadingService);
   private readonly dialogService = inject(DialogService);
   private readonly confirmationService = inject(ConfirmationService);
+  private readonly userAuth = inject(UserAuthService);
 
   // 🔽 Signal to manage loading state
   readonly loading = signal(false);
@@ -71,7 +72,7 @@ export class UserProfileFormComponent implements OnInit {
     { field: 'UserId', col: 'کد کاربری' },
     { field: 'UserName', col: 'نام کاربری' },
     { field: 'UserTypeTitle', col: 'نوع کاربر' },
-    { field: 'MobileNumber', col: 'شماره همراه:' },
+    { field: 'MobileNumber', col: 'شماره همراه' },
     { field: 'UserActive', col: 'وضعیت کاربر' },
     { field: 'SMSOwnerActive', col: 'وضعیت پیامک' },
     { field: 'walletId', col: 'کد کیف پول' },
@@ -168,7 +169,6 @@ export class UserProfileFormComponent implements OnInit {
     const userId = this.userProfile().UserId;
     if (userId === -1) return;
 
-
     const res = await this.userService.ResetSoftwareUserPassword(userId);
     if (!checkAndToastError(res, this.toast)) return;
 
@@ -179,5 +179,9 @@ export class UserProfileFormComponent implements OnInit {
       modal: true,
       inputValues: { username: Username, password: Password },
     });
+  }
+
+  logout() {
+    this.userAuth.logout();
   }
 }
