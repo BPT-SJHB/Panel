74f3b0336c03b05a1df022a4c4532491f1aba02c
rezishon <rezashahmoradi.81@outlook.com>
2025-08-10 17:14:30 +0330

    feat(services/load-management/model)[transport-tariff-param]: add `TransportTariffParam` model

---
+++ src/app/services/load-management/model/transport-tariff-param.model.ts
@@ -0,0 +1,12 @@
+export interface TransportTariffParam {
+  TPTPDId: number;
+  AnnouncementSGId: number;
+  TPTPId: number;
+  Mblgh: number;
+  DateTimeMilladi: string;
+  DateShamsi: string;
+  Time: string;
+  RelationActive: boolean;
+  Checked: boolean;
+  TPTPTitle: string;
+}
