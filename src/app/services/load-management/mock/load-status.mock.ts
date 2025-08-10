bb25e961edd71d5148e4a487580c20f9107bb781
rezishon <rezashahmoradi.81@outlook.com>
2025-08-10 17:12:06 +0330

    feat(services/load-management/mock)[load-status]: add `mockLoadStatuses` mock

---
+++ src/app/services/load-management/mock/load-status.mock.ts
@@ -0,0 +1,34 @@
+import { LoadStatus } from '../model/load-status.model';
+
+export const mockLoadStatuses: LoadStatus[] = [
+  {
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+  },
+  {
+    LoadStatusId: 2,
+    LoadStatusTitle:
+      'بار آزاد                                                                                            ',
+  },
+  {
+    LoadStatusId: 3,
+    LoadStatusTitle:
+      'حذف شده                                                                                             ',
+  },
+  {
+    LoadStatusId: 4,
+    LoadStatusTitle:
+      'کنسل شده                                                                                            ',
+  },
+  {
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+  },
+  {
+    LoadStatusId: 6,
+    LoadStatusTitle:
+      'ثبت شده برای فردا                                                                                   ',
+  },
+];
