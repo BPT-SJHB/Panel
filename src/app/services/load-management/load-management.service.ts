ed2e0685251e9150f812368c3ff7a554f0246b72
rezishon <rezashahmoradi.81@outlook.com>
2025-08-10 17:26:42 +0330

    feat(services/load-management)[load-management]: add `DeleteLoad` method
    Will remove specific load in database by `loadId` as input parameter.

--- src/app/services/load-management/load-management.service.ts
+++ src/app/services/load-management/load-management.service.ts
@@ -189,4 +189,26 @@ export class LoadManagementService {
     >(apiUrl, bodyValue, mockShortResponse);
     //#endregion
   }
+
+  public async DeleteLoad(loadId: number): Promise<ApiResponse<ShortResponse>> {
+    this.userAuth.isLoggedIn();
+
+    //#region Consts
+    const apiUrl = API_ROUTES.LoadCapacitorAPI.DeleteLoad;
+    const loadInfo: LoadInfo = {
+      LoadId: loadId,
+    };
+    const bodyValue = {
+      SessionId: this.userAuth.getSessionId(),
+      LoadId: loadInfo.LoadId,
+    };
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      ShortResponse
+    >(apiUrl, bodyValue, mockShortResponse);
+    //#endregion
+  }
 }
