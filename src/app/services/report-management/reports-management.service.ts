34f4fad2302584abeec788ff3a7b8deb2cfbab39
rezishon <rezashahmoradi.81@outlook.com>
2025-08-11 11:51:06 +0330

    chore(app/service)[report-management]: add needed imports

--- src/app/services/report-management/reports-management.service.ts
+++ src/app/services/report-management/reports-management.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import { LoadPermission } from './model/load/load-permission.model';
+import { API_ROUTES } from 'app/constants/api';
+import { LoadInfo } from '../load-management/model/load-info.model';
+import { mockLoadPermissions } from './mock/load/load-permission.mock';
+import { ApiResponse } from 'app/data/model/api-Response.model';
 
 @Injectable({
   providedIn: 'root'
