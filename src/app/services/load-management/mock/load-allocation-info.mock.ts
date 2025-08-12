25a551b8fa916da9d994f7c457c29fc3bfbe3eb5
rezishon <rezashahmoradi.81@outlook.com>
2025-08-12 09:03:12 +0330

    feat(services/load-management/mock)[load-allocation-info]: add `mockLoadAllocationInfos` mock

---
+++ src/app/services/load-management/mock/load-allocation-info.mock.ts
@@ -0,0 +1,22 @@
+import { LoadAllocationInfo } from '../model/load-allocation-info.model';
+
+export const mockLoadAllocationInfos: LoadAllocationInfo[] = [
+  {
+    LAId: 30031,
+    Priority: 1,
+    LoadAllocationStatusColor:
+      'BLUE                                                                                                ',
+    LoadId: 31,
+    GoodTitle: 'شمش چدن',
+    SourceCityTitle: 'اصفهان - ذوب آهن',
+    TargetCityTitle: 'مشگين شهر',
+    Tonaj: 22,
+    TransportCompanyTitle: 'ا مانت  بار                        ',
+    LoadingPlaceTitle: 'برشکاری حسینی',
+    DischargingPlaceTitle: 'آسیا زرین 1',
+    Description: 'فرداصبح بارگيري',
+    Address: '',
+    Recipient: '',
+    TPTParamsJoint: '',
+  },
+];
