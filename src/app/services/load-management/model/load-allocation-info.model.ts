8dbd41e03bbcb1d6d68159f691bdb73eff1318c9
rezishon <rezashahmoradi.81@outlook.com>
2025-08-12 09:02:40 +0330

    feat(services/load-management/model)[load-allocation-info]: add `LoadAllocationInfo` model

---
+++ src/app/services/load-management/model/load-allocation-info.model.ts
@@ -0,0 +1,17 @@
+export interface LoadAllocationInfo {
+  LAId: number;
+  LoadId: number;
+  Priority?: number;
+  LoadAllocationStatusColor?: string;
+  GoodTitle?: string;
+  SourceCityTitle?: string;
+  TargetCityTitle?: string;
+  Tonaj?: number;
+  TransportCompanyTitle?: string;
+  LoadingPlaceTitle?: string;
+  DischargingPlaceTitle?: string;
+  Description?: string;
+  Address?: string;
+  Recipient?: string;
+  TPTParamsJoint?: string;
+}
