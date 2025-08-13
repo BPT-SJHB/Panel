3c3c7816af76eff0a3a9270b89026edc4373d3ed
rezishon <rezashahmoradi.81@outlook.com>
2025-08-13 13:20:53 +0330

    feat(app/services/load-management/mock)[load-allocated-to-next-turn]: add `mockLoadAllocatedToNextTurn` mock

---
+++ src/app/services/load-management/mock/load-allocated-to-next-turn.mock.ts
@@ -0,0 +1,21 @@
+import { LoadAllocatedToNextTurn } from '../model/load-allocated-to-next-turn.model';
+
+export const mockLoadAllocatedToNextTurn: LoadAllocatedToNextTurn = {
+  TurnId: 19,
+  TruckId: 85187,
+  TurnIssueDate: '1404/05/06',
+  TurnIssueTime: '15:49:12',
+  TurnDescription: 'بلادرنگ',
+  TurnIssueSoftwareUserId: 7004,
+  TurnLastChangedSoftwareUserId: 7004,
+  TruckDriverName: ';محسن;جلالي',
+  bFlag: false,
+  bFlagDriver: false,
+  TurnLastChangedDate: '',
+  TurnLastChangedTime: '',
+  TruckDriverId: 81871,
+  BillOfLadingNumber: '',
+  SequentialTurnNumber: 'T1404/000008',
+  TurnStatusId: 7,
+  RegisteringTimeStamp: '1/1/2015',
+};
