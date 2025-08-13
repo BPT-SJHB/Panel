92c14987ae94ee0241b4b4dde8c85208e48c4dfb
rezishon <rezashahmoradi.81@outlook.com>
2025-08-13 13:24:50 +0330

    feat(app/services/report-management/model/load)[load-permission-for-driver]: add `LoadPermissionForDriver` model

---
+++ src/app/services/report-management/model/load/load-permission-for-driver.model.ts
@@ -0,0 +1,12 @@
+export interface LoadPermissionForDriver {
+  LoadId: number;
+  Tonaj: number;
+  SequentialTurn: string;
+  LoadAllocationId: number;
+  Priority: number;
+  ShamsiDate: string;
+  Time: string;
+  GoodTitle: string;
+  LoadSourceCity: string;
+  LoadTargetCity: string;
+}
