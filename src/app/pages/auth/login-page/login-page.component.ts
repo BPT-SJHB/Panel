7a68dec150414de34562e2f6e51f6838f7e1017a
rezishon <rezashahmoradi.81@outlook.com>
2025-08-17 09:26:37 +0330

    refactor(app/pages/auth)[login-page.*]: Update component style
    The page doesn't work properly well on small screen devices, so I update
    it.

--- src/app/pages/auth/login-page/login-page.component.ts
+++ src/app/pages/auth/login-page/login-page.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 import { LoginFormComponent } from 'app/components/forms/login-form/login-form.component';
 import { FullScreenBackgroundComponent } from 'app/components/shared/full-screen-background/full-screen-background.component';
 import { FormCardComponent } from 'app/components/shared/form-card/form-card.component';
+import { SupportButtonComponent } from 'app/components/shared/support-button/support-button.component';
 
 @Component({
   selector: 'app-login-page',
-  imports: [LoginFormComponent, FullScreenBackgroundComponent, FormCardComponent],
+  imports: [
+    LoginFormComponent,
+    FullScreenBackgroundComponent,
+    FormCardComponent,
+    SupportButtonComponent,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 
 export class LoginPageComponent {
