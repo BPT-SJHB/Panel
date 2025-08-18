13b1ecf0a884cb754427d696d5351d60d2af029f
MohsenBg <moh.1380.1393@gmail.com>
2025-08-18 19:22:08 +0330

    refactor(user-info):fix disable button user-info

--- src/app/components/forms/user-management-form/user-Info-form/user-info-form.component.ts
+++ src/app/components/forms/user-management-form/user-Info-form/user-info-form.component.ts
@@ -80,14 +80,13 @@ export class UserInfoFormComponent extends BaseLoading implements OnInit {
    *  ================================ */
 
   fetchUserByPhone: (phone: string) => Promise<void> = async (
-    phone: string,
+    phone: string
   ) => {
     if (this.searchForm.invalid || this.loading()) return;
 
     await this.withLoading(async () => {
       const response = await this.userManager.GetSoftwareUserInfo(phone);
       if (!checkAndToastError(response, this.toast)) return;
-      console.log(response.data);
 
       this.populateForm(response.data);
     });
@@ -116,7 +115,7 @@ export class UserInfoFormComponent extends BaseLoading implements OnInit {
 
     await this.withLoading(async () => {
       const response = await this.userManager.ResetSoftwareUserPassword(
-        id.value!,
+        id.value!
       );
       if (!checkAndToastError(response, this.toast)) return;
 
@@ -171,11 +170,10 @@ export class UserInfoFormComponent extends BaseLoading implements OnInit {
   }
 
   async submitUserInfo(): Promise<void> {
-    const id = this.getUserFormControl('id');
-    if (id.invalid) {
-      await this.registerUser();
-    } else {
+    if (this.searchForm.valid) {
       await this.updateUserInfo();
+    } else if (this.isFormValidExcept('id')) {
+      await this.registerUser();
     }
   }
 
@@ -248,7 +246,7 @@ export class UserInfoFormComponent extends BaseLoading implements OnInit {
    *  Form control getters (typed access)
    *  ================================ */
   getUserFormControl<K extends keyof UserInfoForm>(
-    name: K,
+    name: K
   ): FormControl<UserInfoForm[K]> {
     const control = this.userInfoForm.get(name as string);
     if (!control) {
@@ -259,5 +257,11 @@ export class UserInfoFormComponent extends BaseLoading implements OnInit {
 
   get searchPhone(): FormControl<string> {
     return this.searchForm.controls.searchPhone as FormControl<string>;
+  }
+
+  isFormValidExcept(exceptControl: string): boolean {
+    return Object.keys(this.userInfoForm.controls)
+      .filter((key) => key !== exceptControl)
+      .every((key) => this.getUserFormControl(key as keyof UserInfoForm).valid);
   }
 }
