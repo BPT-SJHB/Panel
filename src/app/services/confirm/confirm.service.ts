a90439d1ed8c55ec587a426d5f136dcb491ac176
MohsenBg <moh.1380.1393@gmail.com>
2025-09-01 16:31:02 +0330

    feat(confirm-service):add submite message and change other messages need to change

--- src/app/services/confirm/confirm.service.ts
+++ src/app/services/confirm/confirm.service.ts
@@ -9,7 +9,7 @@ export class AppConfirmService {
   confirmDelete(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
       header: 'تأیید حذف',
-      message: `آیا مطمئن هستید که می‌خواهید آیتم <b><u>${itemName}</u></b> را حذف کنید؟`,
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را حذف کنید؟`,
       icon: 'pi pi-exclamation-triangle',
       acceptLabel: 'حذف',
       rejectLabel: 'انصراف',
@@ -19,11 +19,25 @@ export class AppConfirmService {
     });
   }
 
+  // ثبت
+  confirmSubmit(itemName: string, accept: () => void) {
+    this.confirmationService.confirm({
+      header: 'تأیید ثبت',
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را ثبت کنید؟`,
+      icon: 'pi pi-check-circle',
+      acceptLabel: 'ثبت',
+      rejectLabel: 'انصراف',
+      acceptButtonProps: { severity: 'success' },
+      rejectButtonProps: { severity: 'secondary' },
+      accept,
+    });
+  }
+
   // ویرایش
   confirmEdit(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
       header: 'تأیید ویرایش',
-      message: `آیا از ویرایش <b><u>${itemName}</u></b> اطمینان دارید؟`,
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را ویرایش کنید؟`,
       icon: 'pi pi-pencil',
       acceptLabel: 'ویرایش',
       rejectLabel: 'انصراف',
@@ -36,8 +50,8 @@ export class AppConfirmService {
   // آزاد کردن خط
   confirmFreeLine(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
-      header: 'تأیید آزاد کردن خط',
-      message: `آیا از آزاد کردن خط <b><u>${itemName}</u></b> اطمینان دارید؟`,
+      header: 'تایید خط آزاد بار',
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را آزاد کنید`,
       icon: 'pi pi-exclamation-triangle',
       acceptLabel: 'آزاد کردن',
       rejectLabel: 'انصراف',
@@ -50,8 +64,8 @@ export class AppConfirmService {
   // رسوب کردن
   confirmSediment(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
-      header: 'تأیید رسوب کردن بار',
-      message: `آیا از رسوب کردن <b><u>${itemName}</u></b> اطمینان دارید؟`,
+      header: 'تایید رسوب بار',
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را رسوب کنید؟`,
       icon: 'pi pi-box',
       acceptLabel: 'رسوب کردن',
       rejectLabel: 'انصراف',
@@ -65,23 +79,9 @@ export class AppConfirmService {
   confirmCancel(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
       header: 'تأیید کنسلی بار',
-      message: `آیا از کنسلی <b><u>${itemName}</u></b> اطمینان دارید؟`,
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را کنسل کنید؟`,
       icon: 'pi pi-times',
       acceptLabel: 'کنسل',
-      rejectLabel: 'انصراف',
-      acceptButtonProps: { severity: 'danger' },
-      rejectButtonProps: { severity: 'secondary' },
-      accept,
-    });
-  }
-
-  //کنسلی تخصیص بار
-  confirmCancelAllocationLoad(itemName: string, accept: () => void) {
-    this.confirmationService.confirm({
-      header: 'تایید کنسلی بار',
-      message: `آیا کنسلی تخصیص <b><u>${itemName}</u></b> تایید می‌کنید؟`,
-      icon: 'pi pi-times',
-      acceptLabel: 'تایید',
       rejectLabel: 'انصراف',
       acceptButtonProps: { severity: 'danger' },
       rejectButtonProps: { severity: 'secondary' },
@@ -92,10 +92,10 @@ export class AppConfirmService {
   // غیرفعال سازی تعرفه ها
   confirmDisableTarrifs(itemName: string, accept: () => void) {
     this.confirmationService.confirm({
-      header: 'تایید غیرفعال سازی تعرفه ها',
-      message: `آیا از غیر فعال‌سازی <b><u>${itemName}</u></b> اطمینان دارید؟`,
+      header: 'تأیید غیرفعال سازی تعرفه ها',
+      message: `آیا می‌خواهید <b><u>${itemName}</u></b> را غیرفعال کنید؟`,
       icon: 'pi pi-times',
-      acceptLabel: 'تایید',
+      acceptLabel: 'تأیید',
       rejectLabel: 'انصراف',
       acceptButtonProps: { severity: 'danger' },
       rejectButtonProps: { severity: 'secondary' },
@@ -104,13 +104,13 @@ export class AppConfirmService {
   }
 
   // انتخاب
-  ConfirmChoose(
+  confirmChoose(
     itemName: string,
     accept: () => void,
     reject?: () => void
   ): void {
     this.confirmationService.confirm({
-      header: 'تأیید انتخاب آیتم',
+      header: 'تأیید انتخاب',
       message: `آیا می‌خواهید <b><u>${itemName}</u></b> را انتخاب کنید؟`,
       icon: 'pi pi-question-circle',
       acceptLabel: 'بله',
