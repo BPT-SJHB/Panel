4ebc7d6fed577562010cca5996724d0d7b2a216c
MohsenBg <moh.1380.1393@gmail.com>
2025-09-26 23:17:11 +0330

    feat(support-button):add ticket create and track with captcha in support buttons

--- src/app/components/shared/support-button/support-button.component.ts
+++ src/app/components/shared/support-button/support-button.component.ts
@@ -1,12 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ScrollTopModule } from 'primeng/scrolltop';
 import { ButtonModule } from 'primeng/button';
 import { PopoverModule } from 'primeng/popover';
 import { ToastService } from 'app/services/toast-service/toast.service';
-import { CaptchaInputComponent } from '../inputs/captcha-input/captcha-input.component';
-import { TextAreaInputComponent } from '../inputs/text-area-input/text-area-input.component';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonComponent } from '../button/button.component';
+import { copyTextAndToast } from 'app/utils/copy-text';
+import { AppTitles } from 'app/constants/Titles';
+import { TicketGuardCaptchaFormComponent } from 'app/components/forms/tickets-management-form/ticket-guard-captcha-form/ticket-guard-captcha-form.component';
+import { Router } from '@angular/router';
+import { APP_ROUTES } from 'app/constants/routes';
 
 @Component({
   selector: 'app-support-button',
@@ -14,21 +17,24 @@ import { ButtonComponent } from '../button/button.component';
     ScrollTopModule,
     ButtonModule,
     PopoverModule,
-    CaptchaInputComponent,
-    TextAreaInputComponent,
     DialogModule,
     ButtonComponent,
+    TicketGuardCaptchaFormComponent,
   ],
   templateUrl: './support-button.component.html',
   styleUrl: './support-button.component.scss',
 })
 export class SupportButtonComponent {
+  readonly AppTitle = AppTitles;
+  private toast = inject(ToastService);
+  private readonly router = inject(Router);
+  readonly activeCaptcha = signal<boolean>(false);
+  readonly captchaAction = signal<() => void>(() => undefined);
+  readonly APP_ROUTS = APP_ROUTES;
   dialog = false;
   visible = false;
 
-  private toast = inject(ToastService);
-
-  contactList: { title: string; numbers: string[] }[] = [
+  readonly contactList = signal<{ title: string; numbers: string[] }[]>([
     {
       title: 'انجمن صنفی کـارفرمایی کـامیونداران',
       numbers: ['03133869199', '09944433611'],
@@ -41,7 +47,7 @@ export class SupportButtonComponent {
       title: 'انـجـمـن صـنـفـی کـارگـری رانـنـدگـان',
       numbers: ['03133873858', '09134727572'],
     },
-  ];
+  ]);
 
   async showDialog() {
     this.visible = true;
@@ -57,19 +63,17 @@ export class SupportButtonComponent {
   }
 
   async copyToClipboard(text: string) {
-    try {
-      // Normal browsers
-      await navigator.clipboard.writeText(text);
-      this.toast.success('متن در کلیپبورد ذخیره شد', '');
-    } catch (err) {
-      // Mobile browsers
-      const tempTextArea = document.createElement('textarea');
-      tempTextArea.value = text;
-      document.body.appendChild(tempTextArea);
-      tempTextArea.select();
-      document.execCommand('copy');
-      document.body.removeChild(tempTextArea);
-      this.toast.success('متن در کلیپبورد ذخیره شد', '');
-    }
+    copyTextAndToast(text, this.toast);
+  }
+
+  redirectUrl(route: string) {
+    this.captchaAction.set(() => {
+      const url = this.router.serializeUrl(this.router.createUrlTree([route]));
+      window.open(url, '_blank');
+      this.activeCaptcha.set(false);
+      this.dialog = false;
+      this.visible = false;
+    });
+    this.activeCaptcha.set(true);
   }
 }
