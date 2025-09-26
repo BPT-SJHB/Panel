1d815203003a97ff90ae7d1b2c83883bf2fe5b0d
MohsenBg <moh.1380.1393@gmail.com>
2025-09-26 23:03:38 +0330

    feat(captcha-input):add ticket captcha to captcha-input

--- src/app/components/shared/inputs/captcha-input/captcha-input.component.ts
+++ src/app/components/shared/inputs/captcha-input/captcha-input.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, input, Input, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
@@ -7,6 +7,8 @@ import { MessageModule } from 'primeng/message';
 import { CaptchaService } from 'app/services/captcha-service/captcha.service';
 import { ToastService } from 'app/services/toast-service/toast.service';
 import { ButtonModule } from 'primeng/button';
+import { TicketServiceManagementService } from 'app/services/ticket-service-management/ticket-service-management.service';
+import { checkAndToastError } from 'app/utils/api-utils';
 
 @Component({
   selector: 'app-captcha-input',
@@ -24,7 +26,9 @@ import { ButtonModule } from 'primeng/button';
 export class CaptchaInputComponent implements OnInit {
   private captchaService = inject(CaptchaService);
   private toast = inject(ToastService);
+  private ticketService = inject(TicketServiceManagementService);
 
+  readonly captchaServiceType = input<'default' | 'ticket'>('default');
   @Input() sessionIdControl: FormControl = new FormControl('');
   @Input() captchaInputControl: FormControl = new FormControl('');
 
@@ -36,12 +40,29 @@ export class CaptchaInputComponent implements OnInit {
   }
 
   async getCaptchaInformation() {
-    const captcha = await this.captchaService.getCaptcha();
-    if (!captcha.success || !captcha.data) {
-      this.toast.error('خطا', captcha.error?.message ?? '');
-      console.log(captcha.error?.details);
-      return;
+    switch (this.captchaServiceType()) {
+      case 'ticket':
+        await this.getCaptchaTicketService();
+        break;
+      case 'default':
+        await this.getDefaultCaptchaService();
+        break;
+      default:
+        break;
     }
+  }
+
+  async getCaptchaTicketService() {
+    const response = await this.ticketService.GetCaptcha();
+    if (!checkAndToastError(response, this.toast)) return;
+
+    this.imageData = 'data:image/png;base64,' + response.data.image;
+    this.sessionIdControl.setValue(response.data.id);
+  }
+
+  async getDefaultCaptchaService() {
+    const captcha = await this.captchaService.getCaptcha();
+    if (!checkAndToastError(captcha, this.toast)) return;
 
     this.imageData = 'data:image/png;base64,' + captcha.data.imageData;
     this.sessionIdControl.setValue(captcha.data.sessionId);
