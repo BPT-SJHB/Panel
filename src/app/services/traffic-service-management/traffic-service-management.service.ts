bb91e4f9f857e1434a4b6105e94a51c64acc5c87
MohsenBg <moh.1380.1393@gmail.com>
2025-10-26 10:25:38 +0330

    feat(traffic-management)[./src/app/services/traffic-management/]:renamed

--- src/app/services/traffic-service-management/traffic-service-management.service.ts
+++
@@ -1,9 +0,0 @@
-import { inject, Injectable } from '@angular/core';
-import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class TrafficServiceManagementService {
-  private apiCommunicator = inject(APICommunicationManagementService);
-}
