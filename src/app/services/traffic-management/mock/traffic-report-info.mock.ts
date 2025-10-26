89073305b1fecf587dd7a12de71420f0db4a720f
rezishon <rezashahmoradi.81@outlook.com>
2025-10-26 21:37:04 +0330

    feat(model)[/src/app/services/traffic-management/mock/traffic-report-info.mock]: add new `mockTrafficReportInfos` mock

---
+++ src/app/services/traffic-management/mock/traffic-report-info.mock.ts
@@ -0,0 +1,22 @@
+import { TrafficReportInfo } from '../model/traffic-report-info.model';
+
+export const mockTrafficReportInfos: TrafficReportInfo[] = [
+  {
+    EntryExitId: 1000,
+    EntryShamsiDate: '1404/08/01',
+    EntryTime: '00:00:00',
+    EntryTrafficCardNo: '0000090BaK',
+    EntrySoftwareUser:
+      'مرتضي شاهمرادي                                                                                      ',
+    EntryCost: 1000,
+    EntryGateTitle: 'ورودی تریلی',
+    ExitShamsiDate: '1404/08/01',
+    ExitTime: '17:00:00',
+    ExitTrafficCardNo: '0000090BaK',
+    ExitSoftwareUser:
+      'مرتضي شاهمرادي                                                                                      ',
+    ExitCost: 200,
+    ExitGateTitle: 'خروجی تریلی',
+    FlagA: true,
+  },
+];
