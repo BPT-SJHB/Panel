9785ca18bb42e0e5ba70e38e3bd31d6310c831a9
MohsenBg <moh.1380.1393@gmail.com>
2025-10-26 10:52:31 +0330

    feat(traffic-management)[services/traffic-managenet]:add mock and model traffic-card-type

---
+++ src/app/services/traffic-management/mock/traffic-card-type.mock.ts
@@ -0,0 +1,40 @@
+import { TrafficCardType } from '../model/traffic-card-type.model';
+
+export const mockTrafficCardTypes: TrafficCardType[] = [
+  {
+    TrafficCardTypeId: 0,
+    TrafficCardTypeTitle:
+      'نامعلوم                                                                                             ',
+    Active: false,
+  },
+  {
+    TrafficCardTypeId: 1,
+    TrafficCardTypeTitle:
+      'سواری                                                                                               ',
+    Active: true,
+  },
+  {
+    TrafficCardTypeId: 2,
+    TrafficCardTypeTitle:
+      'تریلی                                                                                               ',
+    Active: true,
+  },
+  {
+    TrafficCardTypeId: 3,
+    TrafficCardTypeTitle:
+      'ده چرخ                                                                                              ',
+    Active: true,
+  },
+  {
+    TrafficCardTypeId: 4,
+    TrafficCardTypeTitle:
+      'شش چرخ                                                                                              ',
+    Active: true,
+  },
+  {
+    TrafficCardTypeId: 5,
+    TrafficCardTypeTitle:
+      'جفت                                                                                                 ',
+    Active: false,
+  },
+];
