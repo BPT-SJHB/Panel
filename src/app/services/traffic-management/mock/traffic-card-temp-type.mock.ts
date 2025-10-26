cc556dae9f95ead87ee4152a5d1303bdb89699fe
MohsenBg <moh.1380.1393@gmail.com>
2025-10-26 11:45:32 +0330

    feat(traffic-card-temp-type.mock)[src/app/services/traffic-management/mock/]: add traffic-card-temp-type mock

---
+++ src/app/services/traffic-management/mock/traffic-card-temp-type.mock.ts
@@ -0,0 +1,12 @@
+import { TrafficCardTempType } from '../model/traffic-card-temp-type.model';
+
+export const mockTrafficCardTempTypes: TrafficCardTempType[] = [
+  {
+    TrafficCardTempTypeId: 0,
+    TrafficCardTempTypeTitle: 'دائمی',
+  },
+  {
+    TrafficCardTempTypeId: 1,
+    TrafficCardTempTypeTitle: 'موقت',
+  },
+];
