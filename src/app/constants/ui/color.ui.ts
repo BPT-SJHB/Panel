a84b43b654eb0ec1746142413a7a80aac38d3a2b
MohsenBg <moh.1380.1393@gmail.com>
2025-11-15 17:03:39 +0330

    feat(color.ui)[src/app/constants/ui/color.ui.ts]: add new ui colors

--- src/app/constants/ui/color.ui.ts
+++ src/app/constants/ui/color.ui.ts
@@ -33,3 +33,39 @@ export const colorMap: ReadonlyMap<string, string> = new Map([
     'bg-indigo-100 text-indigo-800 dark:bg-indigo-700 dark:text-indigo-100',
   ],
 ]);
+
+export const BackColor: ReadonlyMap<string, string> = new Map([
+  ['black', 'bg-black dark:bg-gray-900'],
+  ['white', 'bg-white dark:bg-gray-800'],
+  ['red', 'bg-red-100 dark:bg-red-700'],
+  ['green', 'bg-green-100 dark:bg-green-700'],
+  ['yellow', 'bg-yellow-100 dark:bg-yellow-600'],
+  ['blue', 'bg-blue-100 dark:bg-blue-700'],
+  ['orange', 'bg-orange-100 dark:bg-orange-600'],
+  ['purple', 'bg-purple-100 dark:bg-purple-700'],
+  ['pink', 'bg-pink-100 dark:bg-pink-600'],
+  ['brown', 'bg-amber-100 dark:bg-amber-700'],
+  ['gray', 'bg-gray-100 dark:bg-gray-700'],
+  ['cyan', 'bg-cyan-100 dark:bg-cyan-700'],
+  ['lime', 'bg-lime-100 dark:bg-lime-700'],
+  ['teal', 'bg-teal-100 dark:bg-teal-700'],
+  ['indigo', 'bg-indigo-100 dark:bg-indigo-700'],
+]);
+
+export const ForeColor: ReadonlyMap<string, string> = new Map([
+  ['black', 'text-white dark:text-white'],
+  ['white', 'text-black dark:text-white'],
+  ['red', 'text-red-800 dark:text-red-200'],
+  ['green', 'text-green-800 dark:text-green-200'],
+  ['yellow', 'text-yellow-800 dark:text-yellow-200'],
+  ['blue', 'text-blue-800 dark:text-blue-200'],
+  ['orange', 'text-orange-800 dark:text-orange-200'],
+  ['purple', 'text-purple-800 dark:text-purple-200'],
+  ['pink', 'text-pink-800 dark:text-pink-200'],
+  ['brown', 'text-amber-800 dark:text-amber-200'],
+  ['gray', 'text-gray-800 dark:text-gray-200'],
+  ['cyan', 'text-cyan-800 dark:text-cyan-200'],
+  ['lime', 'text-lime-800 dark:text-lime-200'],
+  ['teal', 'text-teal-800 dark:text-teal-200'],
+  ['indigo', 'text-indigo-800 dark:text-indigo-200'],
+]);
