3b067ccea5bf32a2533ab1facabbc2b0d4f0d7a6
rezishon <rezashahmoradi.81@outlook.com>
2025-11-23 18:21:21 +0330

    feat(config-management service): add needed class

---
+++ src/app/services/config-management/config-management.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { UserAuthService } from '../user-auth-service/user-auth.service';
+import { APICommunicationManagementService } from '../api-communication-management/apicommunication-management.service';
+import {
+  DeleteInfoOfLoadAnnouncementConfig,
+  LoadAnnouncementConfig,
+} from './model/load-announcement-config.model';
+import { API_ROUTES } from 'app/constants/api';
+import { mockLoadAnnouncementConfigs } from './mock/load-announcement-config.mock';
+import { ApiResponse } from 'app/data/model/api-Response.model';
+import { ShortResponse } from 'app/data/model/short-response.model';
+import { mockShortResponse } from 'app/data/mock/short-response.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigManagementService {
+  private userAuth = inject(UserAuthService);
+  private apiCommunicator = inject(APICommunicationManagementService);
+}
