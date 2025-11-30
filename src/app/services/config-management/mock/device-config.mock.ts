2e1a2be952b2ade097784aba2585ecc7cbda628f
rezishon <rezashahmoradi.81@outlook.com>
2025-11-30 13:46:58 +0330

    feat(Devices management): add new `mockDevices` mock

---
+++ src/app/services/config-management/mock/device-config.mock.ts
@@ -0,0 +1,34 @@
+import { Device } from '../model/device-config.model';
+
+export const mockDevices: Device[] = [
+  {
+    DeviceId: 1,
+    DeviceTitle: 'ورودي تريلي',
+    DeviceLocation: 'ورودي تريلي',
+    Active: true,
+  },
+  {
+    DeviceId: 2,
+    DeviceTitle: 'خروجی تریلی',
+    DeviceLocation: 'خروجی تریلی',
+    Active: true,
+  },
+  {
+    DeviceId: 3,
+    DeviceTitle: 'سواري',
+    DeviceLocation: '24 غرفه پايانه',
+    Active: true,
+  },
+  {
+    DeviceId: 4,
+    DeviceTitle: 'سواري',
+    DeviceLocation: '24 غرفه پايانه',
+    Active: true,
+  },
+  {
+    DeviceId: 6,
+    DeviceTitle: 'ورودي تريلي',
+    DeviceLocation: 'ورودي تريلي',
+    Active: true,
+  },
+];
