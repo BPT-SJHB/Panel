73a21b48b3bdffeb7ad3e6a6b8d2c31ac333dbbd
rezishon <rezashahmoradi.81@outlook.com>
2025-12-01 19:04:12 +0330

    feat(LoaderType): add `LoaderTypeToAnnouncementSubGroupRelation` mock

---
+++ src/app/services/loader-types/mock/loader-type-announcement-sub-groups-relation.mock.ts
@@ -0,0 +1,49 @@
+import { LoaderTypeToAnnouncementSubGroupRelation } from '../model/loader-type-announcement-sub-groups-relation.model';
+
+export const mockLoaderTypeToAnnouncementSubGroupRelation: LoaderTypeToAnnouncementSubGroupRelation[] =
+  [
+    {
+      LoaderTypeId: 505,
+      LoaderTypeTitle:
+        'کفی 18 چرخ                                                                                          ',
+      AnnouncementSGId: 7,
+      AnnouncementSGTitle:
+        'برون شهری آهن آلات ذوبی                                                                             ',
+      AnnouncementId: 2,
+      AnnouncementTitle:
+        'تريلي برون شهري ذوب و سبا                                                                           ',
+    },
+    {
+      LoaderTypeId: 505,
+      LoaderTypeTitle:
+        'کفی 18 چرخ                                                                                          ',
+      AnnouncementSGId: 8,
+      AnnouncementSGTitle:
+        'برون شهری شمش                                                                                       ',
+      AnnouncementId: 2,
+      AnnouncementTitle:
+        'تريلي برون شهري ذوب و سبا                                                                           ',
+    },
+    {
+      LoaderTypeId: 505,
+      LoaderTypeTitle:
+        'کفی 18 چرخ                                                                                          ',
+      AnnouncementSGId: 9,
+      AnnouncementSGTitle:
+        'برون شهری صادراتی                                                                                   ',
+      AnnouncementId: 2,
+      AnnouncementTitle:
+        'تريلي برون شهري ذوب و سبا                                                                           ',
+    },
+    {
+      LoaderTypeId: 605,
+      LoaderTypeTitle:
+        'بغلدار چادری 18چرخ                                                                                  ',
+      AnnouncementSGId: 7,
+      AnnouncementSGTitle:
+        'برون شهری آهن آلات ذوبی                                                                             ',
+      AnnouncementId: 2,
+      AnnouncementTitle:
+        'تريلي برون شهري ذوب و سبا                                                                           ',
+    },
+  ];
