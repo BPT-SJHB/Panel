a0cd44c6c90485e08521ad239bb7526effa747de
rezishon <rezashahmoradi.81@outlook.com>
2025-12-01 19:04:23 +0330

    feat(LoaderType): add `LoaderTypeToAnnouncementSubGroupRelation` methods

--- src/app/services/loader-types/loader-types.service.ts
+++ src/app/services/loader-types/loader-types.service.ts
@@ -7,6 +7,11 @@ import { API_ROUTES } from 'app/constants/api';
 import { ShortResponse } from 'app/data/model/short-response.model';
 import { mockShortResponse } from 'app/data/mock/short-response.mock';
 import { mockLoaderTypes } from './mock/loader-type.mock';
+import {
+  LoaderTypeToAnnouncementSubGroupRelation,
+  RegisterDeleteRelationToAnnouncementSubGroup,
+} from './model/loader-type-announcement-sub-groups-relation.model';
+import { mockLoaderTypeToAnnouncementSubGroupRelation } from './mock/loader-type-announcement-sub-groups-relation.mock';
 
 @Injectable({
   providedIn: 'root',
@@ -79,4 +84,72 @@ export class LoaderTypesService {
     >(apiUrl, bodyValue, mockShortResponse);
     //#endregion
   }
+
+  //#region RelationToAnnouncementSubGroups
+
+  public async GetLoaderTypeRelationsToAnnouncementSubGroups(): Promise<
+    ApiResponse<LoaderTypeToAnnouncementSubGroupRelation[]>
+  > {
+    //#region Consts
+    const apiUrl =
+      API_ROUTES.TransportationAPI.LoaderTypes
+        .GetRelationsToAnnouncementSubGroups;
+    const bodyValue = {
+      SessionId: this.userAuth.getSessionId(),
+    };
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      LoaderTypeToAnnouncementSubGroupRelation[]
+    >(apiUrl, bodyValue, mockLoaderTypeToAnnouncementSubGroupRelation);
+    //#endregion
+  }
+
+  public async RegisterRelationToAnnouncementSubGroup(
+    relationInfo: RegisterDeleteRelationToAnnouncementSubGroup
+  ): Promise<ApiResponse<ShortResponse>> {
+    //#region Consts
+    const apiUrl =
+      API_ROUTES.TransportationAPI.LoaderTypes
+        .RegisterRelationToAnnouncementSubGroup;
+    const bodyValue = {
+      SessionId: this.userAuth.getSessionId(),
+      LoaderTypeId: relationInfo.LoaderTypeId,
+      AnnouncementSubGroupId: relationInfo.AnnouncementSGId,
+    };
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      ShortResponse
+    >(apiUrl, bodyValue, mockShortResponse);
+    //#endregion
+  }
+
+  public async DeleteRelationToAnnouncementSubGroup(
+    relationInfo: RegisterDeleteRelationToAnnouncementSubGroup
+  ): Promise<ApiResponse<ShortResponse>> {
+    //#region Consts
+    const apiUrl =
+      API_ROUTES.TransportationAPI.LoaderTypes
+        .DeleteRelationToAnnouncementSubGroup;
+    const bodyValue = {
+      SessionId: this.userAuth.getSessionId(),
+      LoaderTypeId: relationInfo.LoaderTypeId,
+      AnnouncementSubGroupId: relationInfo.AnnouncementSGId,
+    };
+    //#endregion
+
+    //#region Request + Return
+    return await this.apiCommunicator.CommunicateWithAPI_Post<
+      typeof bodyValue,
+      ShortResponse
+    >(apiUrl, bodyValue, mockShortResponse);
+    //#endregion
+  }
+
+  //#endregion
 }
