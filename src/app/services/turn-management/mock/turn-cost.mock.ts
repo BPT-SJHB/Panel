4da24dc06a9af73fc4c5e8a0acef6064bd06212a
rezishon <rezashahmoradi.81@outlook.com>
2025-12-06 12:59:48 +0330

    feat(TurnCosts): Add new mock
    - mockTurnCosts

---
+++ src/app/services/turn-management/mock/turn-cost.mock.ts
@@ -0,0 +1,44 @@
+import { TurnCost } from '../model/turn-cost.model';
+
+export const mockTurnCosts: TurnCost[] = [
+  {
+    SeqTurnId: 1,
+    SeqTurnTitle:
+      'اطاقدار شوينده                                                                                      ',
+    SelfGoverCost: 350000,
+    TruckersAssociationCost: 50000,
+    TruckDriversAssociationCost: 50000,
+  },
+  {
+    SeqTurnId: 2,
+    SeqTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    SelfGoverCost: 400000,
+    TruckersAssociationCost: 30000,
+    TruckDriversAssociationCost: 30000,
+  },
+  {
+    SeqTurnId: 3,
+    SeqTurnTitle:
+      'تریلی شهری - آهن آلات انباری و ذوبی ، رول سبا                                                       ',
+    SelfGoverCost: 350000,
+    TruckersAssociationCost: 50000,
+    TruckDriversAssociationCost: 50000,
+  },
+  {
+    SeqTurnId: 7,
+    SeqTurnTitle:
+      'شهري - شمش کوهپايه                                                                                  ',
+    SelfGoverCost: 320000,
+    TruckersAssociationCost: 40000,
+    TruckDriversAssociationCost: 40000,
+  },
+  {
+    SeqTurnId: 8,
+    SeqTurnTitle:
+      'مرتضي شاهمرادي                                                                                      ',
+    SelfGoverCost: 720000,
+    TruckersAssociationCost: 40000,
+    TruckDriversAssociationCost: 40000,
+  },
+];
