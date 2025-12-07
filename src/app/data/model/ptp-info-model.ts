e39352320c622392d953327034a424ca24abdebf
rezishon <rezashahmoradi.81@outlook.com>
2025-12-07 17:53:42 +0330

    fix(TPTParams): remove old interface
    This interface just removed duo to old use.

--- src/app/data/model/ptp-info-model.ts
+++
@@ -1,6 +0,0 @@
-export interface PTPInfo {
-  TPTPDId: number; // کد پارامتر های ماثر در حمل بار
-  TPTPTitle: string; // عنوان پارامتر های ماثر در حمل بار
-  Cost: number; // مبلغ
-  Checked: boolean; // فعال یا غیرفعال
-}
