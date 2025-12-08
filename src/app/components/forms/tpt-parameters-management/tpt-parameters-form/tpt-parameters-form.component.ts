e64e7990989c9b7dfc30108d04b9dda8695a8aaf
MohsenBg <moh.1380.1393@gmail.com>
2025-12-08 12:24:36 +0330

    feat(tpt-params-form):rename files to tpt-params and add api service

--- src/app/components/forms/tpt-parameters-management/tpt-parameters-form/tpt-parameters-form.component.ts
+++ src/app/components/forms/tpt-parameters-management/tpt-parameters-form/tpt-parameters-form.component.ts
@@ -9,21 +9,18 @@ import {
 } from 'app/components/shared/table/table.component';
 import { ValidationSchema } from 'app/constants/validation-schema';
 import { AppConfirmService } from 'app/services/confirm/confirm.service';
-import { TransportCompaniesManagementService } from 'app/services/transport-company-management/transport-companies-management.service';
 import { BaseLoading } from '../../shared/component-base/base-loading';
 import { checkAndToastError } from 'app/utils/api-utils';
-import { mockShortResponse } from 'app/data/mock/short-response.mock';
 import { ButtonComponent } from 'app/components/shared/button/button.component';
 import { TextInputComponent } from 'app/components/shared/inputs/text-input/text-input.component';
 import { Dialog } from 'primeng/dialog';
+import { TPTParamsManagementService } from 'app/services/tpt-params-management/tptparams-management.service';
+import { TPTParamInfo } from 'app/services/tpt-params-management/model/tptparam-info.model';
+import { OnViewActivated } from 'app/interfaces/on-view-activated.interface';
+import { deepNullToUndefined } from 'app/utils/null-to-undefined';
 
 // ------------------ INTERFACES & TYPES ------------------
-export interface TPTParams {
-  TPTPId: number;
-  TPTPTitle: string;
-}
-
-type TPTParamsRow = TPTParams & {
+type TPTParamsRow = TPTParamInfo & {
   edit: string;
   delete: string;
 };
@@ -34,92 +31,19 @@ enum FormMode {
   REGISTER,
 }
 
-// ------------------ MOCK DATA ------------------
-export const mockTPTParams: TPTParams[] = [
-  { TPTPId: 1, TPTPTitle: 'دو باسکوله (ذوب آهنی)' },
-  { TPTPId: 2, TPTPTitle: 'سه باسکوله (ذوب آهنی)' },
-  { TPTPId: 3, TPTPTitle: 'چهار باسکوله (ذوب آهنی)' },
-  { TPTPId: 4, TPTPTitle: 'شب آخر' },
-  { TPTPId: 7, TPTPTitle: 'کلاف' },
-  { TPTPId: 8, TPTPTitle: 'پروژه' },
-  { TPTPId: 10, TPTPTitle: 'دومحل تخلیه (توافقی)' },
-  { TPTPId: 11, TPTPTitle: 'تیرآهن نرمال' },
-  { TPTPId: 12, TPTPTitle: 'میلگرد نرمال' },
-  { TPTPId: 13, TPTPTitle: 'طرح شهری (روز تخلیه)' },
-  { TPTPId: 14, TPTPTitle: 'طرح شهری محل خاص و پروژه  (شب تخلیه)' },
-  { TPTPId: 15, TPTPTitle: 'طرح (شهرستان)' },
-  {
-    TPTPId: 16,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار نزدیک بدون جابجایی)',
-  },
-  {
-    TPTPId: 17,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار دور بدون جابجایی)',
-  },
-  {
-    TPTPId: 18,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار نزدیک با جابجایی 50%)',
-  },
-  {
-    TPTPId: 19,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار نزدیک با جابجایی 70%)',
-  },
-  {
-    TPTPId: 20,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار دور با جابجایی 50%)',
-  },
-  {
-    TPTPId: 21,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (انبار دور با جابجایی 70%)',
-  },
-  {
-    TPTPId: 22,
-    TPTPTitle: 'دو محل بارگیری ذوب و انبار (جابجایی 100% = کرایه کامل شهری)',
-  },
-  {
-    TPTPId: 23,
-    TPTPTitle:
-      'دو محل بارگیری ذوب و انبار (انبار دور با فاصله غیر متعارف = توافق دو صنف)',
-  },
-  { TPTPId: 24, TPTPTitle: 'دو محل بارگیری انباری (انبار نزدیک بدون جابجایی)' },
-  { TPTPId: 25, TPTPTitle: 'دو محل بارگیری انباری (انبار دور بدون جابجایی)' },
-  {
-    TPTPId: 26,
-    TPTPTitle: 'دو محل بارگیری انباری (انبار نزدیک با جابجایی 50%)',
-  },
-  {
-    TPTPId: 27,
-    TPTPTitle: 'دو محل بارگیری انباری (انبار نزدیک با جابجایی 70%)',
-  },
-  {
-    TPTPId: 28,
-    TPTPTitle: 'دو محل بارگیری انباری (انبار نزدیک با جابجایی 100%)',
-  },
-  { TPTPId: 29, TPTPTitle: 'دو محل بارگیری انباری (انبار دور با جابجایی 50%)' },
-  { TPTPId: 30, TPTPTitle: 'دو محل بارگیری انباری (انبار دور با جابجایی 70%)' },
-  {
-    TPTPId: 31,
-    TPTPTitle: 'دو محل بارگیری انباری (انبار دور با جابجایی 100%)',
-  },
-  {
-    TPTPId: 32,
-    TPTPTitle:
-      'دو محل بارگیری انباری (انبار دور با فاصله غیر متعارف = توافق دو صنف)',
-  },
-];
-
 // ------------------ COMPONENT ------------------
 @Component({
-  selector: 'app-effective-parameters-form',
+  selector: 'app-tpt-parameters-form',
   imports: [TableComponent, Dialog, TextInputComponent, ButtonComponent],
-  templateUrl: './effective-parameters-form.component.html',
-  styleUrl: './effective-parameters-form.component.scss',
+  templateUrl: './tpt-parameters-form.component.html',
+  styleUrl: './tpt-parameters-form.component.scss',
 })
-export class EffectiveParametersFormComponent extends BaseLoading {
+export class TPTParametersFormComponent
+  extends BaseLoading
+  implements OnViewActivated
+{
   // ------------------ INJECTED SERVICES ------------------
-  private readonly transportCompanyService = inject(
-    TransportCompaniesManagementService
-  );
+  private readonly tptParamsService = inject(TPTParamsManagementService);
   private readonly fb = inject(FormBuilder);
   private readonly confirmService = inject(AppConfirmService);
 
@@ -157,7 +81,7 @@ export class EffectiveParametersFormComponent extends BaseLoading {
   // ------------------ DATA METHODS ------------------
   private async loadTPTParams() {
     await this.withLoading(async () => {
-      const response = { success: true, data: mockTPTParams };
+      const response = await this.tptParamsService.GetAllTPTParams();
       if (!checkAndToastError(response, this.toast)) return;
 
       const rows = response.data.map((d) => ({
@@ -170,17 +94,16 @@ export class EffectiveParametersFormComponent extends BaseLoading {
   }
 
   // ------------------ CRUD ACTIONS ------------------
-  private async onDelete(row: TPTParams) {
+  private async onDelete(row: TPTParamInfo) {
     this.confirmService.confirmDelete(
       `پارامتر موثر با شناسه ${row.TPTPId} و عنوان ${row.TPTPTitle}`,
       async () => {
         if (this.loading()) return;
 
         const deletePayload = this.serializeTPTParams(row);
-        console.log({ deletePayload });
 
         await this.withLoading(async () => {
-          const res = { success: true, data: mockShortResponse };
+          const res = await this.tptParamsService.DeleteTPTParam(deletePayload);
           if (!checkAndToastError(res, this.toast)) return;
           this.toast.success('موفق', res.data.Message);
         });
@@ -205,31 +128,30 @@ export class EffectiveParametersFormComponent extends BaseLoading {
   private async submitEdit() {
     const editPayload = this.serializeTPTParams({
       ...this.form.getRawValue(),
-    } as TPTParams);
+    } as TPTParamInfo);
 
-    console.log({ editPayload });
-
-    const res = { success: true, data: mockShortResponse };
+    const res = await this.tptParamsService.EditTPTParam(editPayload);
     if (!checkAndToastError(res, this.toast)) return;
     this.toast.success('موفق', res.data.Message);
   }
 
   private async submitRegister() {
     await this.withLoading(async () => {
-      const registerPayload = this.serializeTPTParams({
+      const registerPayload = {
         ...this.form.getRawValue(),
-      } as TPTParams);
+      } as TPTParamInfo;
 
-      console.log({ registerPayload });
+      const res = await this.tptParamsService.RegisterTPTParam(
+        deepNullToUndefined(registerPayload)
+      );
 
-      const res = { success: true, data: mockShortResponse };
       if (!checkAndToastError(res, this.toast)) return;
       this.toast.success('موفق', res.data.Message);
     });
   }
 
   // ------------------ UTILITIES ------------------
-  serializeTPTParams(obj: TPTParams): TPTParams {
+  serializeTPTParams(obj: TPTParamInfo) {
     return {
       TPTPId: Number(obj.TPTPId),
       TPTPTitle: obj.TPTPTitle,
@@ -241,13 +163,13 @@ export class EffectiveParametersFormComponent extends BaseLoading {
   }
 
   onNew() {
-    this.form.reset();
+    this.form.reset({ TPTPId: null });
     this.formMode.set(FormMode.REGISTER);
     this.dialogTitle.set('ثبت پارامتر موثر');
     this.isDialogVisible = true;
   }
 
-  onEdit(row: TPTParams) {
+  onEdit(row: TPTParamInfo) {
     this.form.reset();
     this.form.patchValue({ ...row });
     this.formMode.set(FormMode.EDITABLE);
