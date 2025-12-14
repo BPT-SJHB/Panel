e95233267269a051d997317409d31b1fbca9a59a
rezishon <rezashahmoradi.81@outlook.com>
2025-12-14 21:12:26 +0330

    feat(TruckNativenessType): add `TruckNativenessType` mock
    Added `mockTruckNativenessTypesInfo` const.

--- src/app/services/driver-truck-management/mock/truck-nativeness-info.mock.ts
+++ src/app/services/driver-truck-management/mock/truck-nativeness-info.mock.ts
@@ -1,7 +1,21 @@
-import { TruckNativenessInfo } from '../model/truck-nativeness-info.model';
+import {
+  TruckNativenessInfo,
+  TruckNativenessType,
+} from '../model/truck-nativeness-info.model';
 
 export const mockTruckNativenessInfo: TruckNativenessInfo = {
   TruckNativenessTypeId: 2,
   TruckNativenessTypeTitle: 'بومی',
   TruckNativenessExpireDate: '1404/07/21',
 };
+
+export const mockTruckNativenessTypesInfo: TruckNativenessType[] = [
+  {
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+  },
+  {
+    TruckNativenessTypeId: 2,
+    TruckNativenessTypeTitle: 'غیر بومی',
+  },
+];
