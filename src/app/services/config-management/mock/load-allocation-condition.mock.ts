278e39c0a74490ae87b97fccc6518fee6006e683
rezishon <rezashahmoradi.81@outlook.com>
2025-12-14 21:42:10 +0330

    feat(LoadAllocationCondition): add `mockLoadAllocationConditionInfo` mock

---
+++ src/app/services/config-management/mock/load-allocation-condition.mock.ts
@@ -0,0 +1,400 @@
+import { LoadAllocationConditionInfo } from '../model/load-allocation-condition-info.model';
+
+export const mockLoadAllocationConditionsInfo: LoadAllocationConditionInfo[] = [
+  {
+    LoadAllocationConditionId: 3,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 1,
+    RequesterTitle: 'محل درخواست نوبت بلادرنگ',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 10004,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 10089,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 7,
+    TurnStatusTitle:
+      'تخصیص صادر شده                                                                                      ',
+  },
+  {
+    LoadAllocationConditionId: 10090,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 8,
+    TurnStatusTitle:
+      'احیاشده - کنسلی مجوز بارگیری                                                                        ',
+  },
+  {
+    LoadAllocationConditionId: 10091,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 9,
+    TurnStatusTitle:
+      'احیاشده - کنسلی تخصیص بار                                                                           ',
+  },
+  {
+    LoadAllocationConditionId: 10092,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 10,
+    TurnStatusTitle:
+      'احیاشده - کاربر                                                                                     ',
+  },
+  {
+    LoadAllocationConditionId: 30090,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 7,
+    RequesterTitle: 'محل درخواست تخصیص بار شرکت حمل و نقل',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 30089,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 7,
+    RequesterTitle: 'محل درخواست تخصیص بار شرکت حمل و نقل',
+    TurnStatusId: 7,
+    TurnStatusTitle:
+      'تخصیص صادر شده                                                                                      ',
+  },
+  {
+    LoadAllocationConditionId: 30091,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 8,
+    RequesterTitle: 'محل درخواست تخصیص بار ادمین سیستم',
+    TurnStatusId: 7,
+    TurnStatusTitle:
+      'تخصیص صادر شده                                                                                      ',
+  },
+  {
+    LoadAllocationConditionId: 40092,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 9,
+    RequesterTitle: 'محل درخواست تخصیص بار در کنسلی مجوز بارگیری',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 40093,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 9,
+    RequesterTitle: 'محل درخواست تخصیص بار در کنسلی مجوز بارگیری',
+    TurnStatusId: 7,
+    TurnStatusTitle:
+      'تخصیص صادر شده                                                                                      ',
+  },
+  {
+    LoadAllocationConditionId: 4,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+    RequesterId: 1,
+    RequesterTitle: 'محل درخواست نوبت بلادرنگ',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 10005,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 50092,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 7,
+    TurnStatusTitle:
+      'تخصیص صادر شده                                                                                      ',
+  },
+  {
+    LoadAllocationConditionId: 30092,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 1,
+    TruckNativenessTypeTitle: 'بومی',
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+    RequesterId: 7,
+    RequesterTitle: 'محل درخواست تخصیص بار شرکت حمل و نقل',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 5,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 2,
+    TruckNativenessTypeTitle: 'غیر بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 1,
+    RequesterTitle: 'محل درخواست نوبت بلادرنگ',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 10006,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 2,
+    TruckNativenessTypeTitle: 'غیر بومی',
+    LoadStatusId: 1,
+    LoadStatusTitle:
+      'ثبت شده                                                                                             ',
+    RequesterId: 6,
+    RequesterTitle: 'محل درخواست تخصیص بار در لیست بار راننده',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+  {
+    LoadAllocationConditionId: 6,
+    AnnouncementId: 2,
+    AnnouncementTitle:
+      'تريلي برون شهري ذوب و سبا                                                                           ',
+    AnnouncementSGId: 7,
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    SequentialTurnId: 2,
+    SequentialTurnTitle:
+      'تريلي برون شهري - آهن آلات انباري و ذوبي ، رول سبا                                                  ',
+    TruckNativenessTypeId: 2,
+    TruckNativenessTypeTitle: 'غیر بومی',
+    LoadStatusId: 5,
+    LoadStatusTitle:
+      'رسوب شده                                                                                            ',
+    RequesterId: 1,
+    RequesterTitle: 'محل درخواست نوبت بلادرنگ',
+    TurnStatusId: 1,
+    TurnStatusTitle:
+      'صادر شده                                                                                            ',
+  },
+];
