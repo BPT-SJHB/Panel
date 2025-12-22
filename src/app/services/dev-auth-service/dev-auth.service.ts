536953ae84c3c915f0a78c777c4412bc4f91b9c2
MohsenBg <moh.1380.1393@gmail.com>
2025-12-22 13:37:17 +0330

    feat(dev-auth): add sesstion base login

--- src/app/services/dev-auth-service/dev-auth.service.ts
+++ src/app/services/dev-auth-service/dev-auth.service.ts
@@ -1,6 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { UserAuthService } from '../user-auth-service/user-auth.service';
 
+enum LoginType {
+  PASSWORD,
+  SESSTION,
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,20 +15,45 @@ export class DevAuthService {
   private readonly _captcha = '123456';
   private readonly password = '123';
   private readonly _adminUsername = '09132043148';
+  private readonly _driverUsername = '09130853148';
+  private readonly _companyUsername = '09131118842';
+
+  private readonly loginType: LoginType = LoginType.SESSTION;
+
+  private readonly adminSesstionId =
+    'c244e7168fe85bf2a255a6c813bbdb70T)PTe^*V4FT2';
+
+  private readonly driverSesstionId =
+    '8ff5309ad269544774e813b0b8280566WwIUn$FIU#f$';
+
+  private readonly companySesstionId =
+    '3ec4d27ee1fcbe9fb659921c2ad39e70kYXjD30Hk@Q#';
 
   public get adminUsername() {
     return this._adminUsername;
   }
 
+  public get driverUsername() {
+    return this._driverUsername;
+  }
+  public get companyUsername() {
+    return this._companyUsername;
+  }
+
   public get sessionId() {
     return this._sessionId;
   }
+
   public get captcha() {
     return this._captcha;
   }
 
   async loginAsAdmin() {
-    const username = '09132043148';
+    if (LoginType.SESSTION === this.loginType) {
+      await this.loginWithSesstion(this.adminSesstionId);
+      return;
+    }
+    const username = this.adminUsername;
     const response = await this.login({
       username: username,
       password: this.password,
@@ -38,7 +68,11 @@ export class DevAuthService {
   }
 
   async loginAsDriver() {
-    const username = this.adminUsername;
+    if (LoginType.SESSTION === this.loginType) {
+      await this.loginWithSesstion(this.driverSesstionId);
+      return;
+    }
+    const username = this.driverUsername;
     const response = await this.login({
       username: username,
       password: this.password,
@@ -53,7 +87,12 @@ export class DevAuthService {
   }
 
   async loginAsCompany() {
-    const username = '09962964696';
+    if (LoginType.SESSTION === this.loginType) {
+      await this.loginWithSesstion(this.companySesstionId);
+      return;
+    }
+
+    const username = this.companyUsername;
     const response = await this.login({
       username: username,
       password: this.password,
@@ -63,7 +102,6 @@ export class DevAuthService {
       console.error(response.error);
       throw new Error(`cannot login as company with user: ${username}`);
     }
-
     return response;
   }
 
@@ -81,5 +119,10 @@ export class DevAuthService {
     });
 
     return response;
+  }
+
+  // TODO: Remove when session move to cookie base server side
+  async loginWithSesstion(sessionId: string) {
+    await this.userAuth.setSessionId(sessionId, true);
   }
 }
