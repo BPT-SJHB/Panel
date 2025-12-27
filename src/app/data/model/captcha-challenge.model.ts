e1dba8d89327abb59b36183331fc72fd6d34974f
rezishon <rezashahmoradi.81@outlook.com>
2025-12-27 21:42:45 +0330

    feat(captcha-challenge.model): add `zodCaptcha` const type

--- src/app/data/model/captcha-challenge.model.ts
+++ src/app/data/model/captcha-challenge.model.ts
@@ -1,4 +1,11 @@
+import { z } from 'zod';
+
 export interface Captcha {
   SessionId: string;
   Captcha: string;
 }
+
+export const zodCaptcha = z.object({
+  SessionId: z.string(),
+  Captcha: z.string(),
+});
