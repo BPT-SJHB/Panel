661977527d772c304832c946cd0f0c664fd23005
rezishon <rezashahmoradi.81@outlook.com>
2025-12-27 13:34:16 +0330

    test(api-processes): add `ApiProcesses` service test file

---
+++ src/app/services/api-processes/api-processes.service.spec.ts
@@ -0,0 +1,28 @@
+import { TestBed } from '@angular/core/testing';
+import { DevAuthService } from '../dev-auth-service/dev-auth.service';
+import { z } from 'zod';
+import { PageGroup, zodPageGroup } from 'app/data/model/page-group.model';
+import { zodError } from 'app/data/model/api-Response.model';
+import { ApiProcessesService } from './api-processes.service';
+import {
+  createApiResponseSchema,
+  validateResponse,
+} from 'app/utils/validate-response.test.utils.spec';
+
+const ApiPageGroupSchema = createApiResponseSchema(z.array(zodPageGroup));
+
+describe('ApiProcessesService', () => {
+  let service: ApiProcessesService;
+  let devAuth: DevAuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [],
+    });
+
+    service = TestBed.inject(ApiProcessesService);
+    devAuth = TestBed.inject(DevAuthService);
+
+    devAuth.logout();
+  });
+});
