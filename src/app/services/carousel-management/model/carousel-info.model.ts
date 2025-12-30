049621de7cbe6b09318d0b6b7a53c01dd4d836de
rezishon <rezashahmoradi.81@outlook.com>
2025-12-30 21:51:43 +0330

    feat(carousel-info.model): add `CarouselInfo` zod const

--- src/app/services/carousel-management/model/carousel-info.model.ts
+++ src/app/services/carousel-management/model/carousel-info.model.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface CarouselInfo {
   CId: number;
   CTitle: string;
@@ -9,3 +11,15 @@ export interface CarouselInfo {
   Active: boolean;
   Picture?: string;
 }
+
+export const zodCarouselInfo = z.object({
+  CId: z.number(),
+  CTitle: z.string(),
+  URL: z.string(),
+  Description: z.string(),
+  DateTimeMilladi: z.string(),
+  ShamsiDate: z.string(),
+  Time: z.string(),
+  Active: z.boolean(),
+  Picture: z.string().optional(),
+});
