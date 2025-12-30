f1bae2e5ced6ce1eb2f46e25186b2a400ff3d691
rezishon <rezashahmoradi.81@outlook.com>
2025-12-30 22:52:30 +0330

    feat(truck-info.model): add `TruckInfo` zod const

--- src/app/services/driver-truck-management/model/truck-info.model.ts
+++ src/app/services/driver-truck-management/model/truck-info.model.ts
@@ -1,6 +1,7 @@
 import { TruckDriverInfo } from './truck-driver-info.model';
 import { Turn } from '../../turn-management/model/turn.model';
 import { Wallet } from '../../wallet-management/model/wallet.model';
+import { z } from 'zod';
 
 export interface TruckInfo {
   TruckId: number;
@@ -9,6 +10,14 @@ export interface TruckInfo {
   Serial?: string;
   SmartCardNo?: string;
 }
+
+export const zodTruckInfo = z.object({
+  TruckId: z.number(),
+  LoaderTypeId: z.number().optional(),
+  Pelak: z.string().optional(),
+  Serial: z.string().optional(),
+  SmartCardNo: z.string().optional(),
+});
 
 export interface TruckComposedInfo {
   Truck: TruckInfo;
