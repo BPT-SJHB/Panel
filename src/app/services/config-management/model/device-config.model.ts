f9aa0f85fcf89bbd7ffb34d8b7e63b7b08fb48e4
rezishon <rezashahmoradi.81@outlook.com>
2025-12-30 22:01:58 +0330

    feat(device-config.model): add `DeviceConfig` zod const

--- src/app/services/config-management/model/device-config.model.ts
+++ src/app/services/config-management/model/device-config.model.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 export interface DeviceConfig {
   CODId: number;
   CODName: string;
@@ -9,6 +11,18 @@ export interface DeviceConfig {
   Description: string;
   CODValue: string;
 }
+
+export const zodDeviceConfig = z.object({
+  CODId: z.number(),
+  CODName: z.string(),
+  CODTitle: z.string(),
+  CODIndex: z.number(),
+  CODIndexTitle: z.string(),
+  DeviceId: z.number(),
+  DeviceTitle: z.string(),
+  Description: z.string(),
+  CODValue: z.string(),
+});
 
 export type EditInfoOfDeviceConfig = Pick<
   DeviceConfig,
