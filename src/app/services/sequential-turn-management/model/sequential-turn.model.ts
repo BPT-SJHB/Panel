1d076c56103c2081c18420f50ea5df0802719f04
rezishon <rezashahmoradi.81@outlook.com>
2026-01-02 01:22:01 +0330

    feat(sequential-turn.model): add `SequentialTurn` zod const

--- src/app/services/sequential-turn-management/model/sequential-turn.model.ts
+++ src/app/services/sequential-turn-management/model/sequential-turn.model.ts
@@ -1,6 +1,15 @@
+import z from 'zod';
+
 export interface SequentialTurn {
   SeqTurnId: number;
   SeqTurnTitle?: string;
   SeqTurnKeyWord?: string;
   Active?: boolean;
 }
+
+export const zodSequentialTurn = z.object({
+  SeqTurnId: z.number(),
+  SeqTurnTitle: z.string().optional(),
+  SeqTurnKeyWord: z.string().optional(),
+  Active: z.boolean().optional(),
+});
