9d5afab4b8ec15d3e055f92b33a614ad3a7eb22f
rezishon <rezashahmoradi.81@outlook.com>
2026-01-02 01:14:17 +0330

    feat(load-permission-for-company.model): add `LoadPermissionForCompany` zod const

--- src/app/services/report-management/model/load-permissions/load-permission-for-company.model.ts
+++ src/app/services/report-management/model/load-permissions/load-permission-for-company.model.ts
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 export interface LoadPermissionForCompany {
   LoadAllocationId: number;
   Pelak: string;
@@ -15,3 +17,21 @@ export interface LoadPermissionForCompany {
   LoadDescription: string;
   SoftwareUserAllocater: string;
 }
+
+export const zodLoadPermissionForCompany = z.object({
+  LoadAllocationId: z.number(),
+  Pelak: z.string(),
+  Serial: z.string(),
+  TruckSmartCardNo: z.string(),
+  Driver: z.string(),
+  TruckDriverNationalCode: z.string(),
+  TruckDriverDrivingLicenseNo: z.string(),
+  TruckDriverTel: z.string(),
+  LoadAllocationDateTime: z.string(),
+  LoadId: z.number(),
+  SourceCityTitle: z.string(),
+  TargetCityTitle: z.string(),
+  ProductTitle: z.string(),
+  LoadDescription: z.string(),
+  SoftwareUserAllocater: z.string(),
+});
