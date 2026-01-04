4854a878b46488832e3db3c44ef2a5c9697c1f14
rezishon <rezashahmoradi.81@outlook.com>
2026-01-04 16:13:44 +0330

    test(fpc-management): add imports

---
+++ src/app/services/fpc-management/fpc-management.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  createApiResponseSchema,
+  validateResponse,
+} from 'app/utils/validate-response.test.utils.spec';
+import { DevAuthService } from '../dev-auth-service/dev-auth.service';
+import { FpcManagementService } from './fpc-management.service';
+import z from 'zod';
+import { FPCInfo, zodFPCInfo } from 'app/data/model/fpc-info.model';
+import { ApiShortResponseSchema } from 'app/data/model/short-response.model.spec';
+import { ShortResponse } from 'app/data/model/short-response.model';
+import {
+  UsernamePassword,
+  zodUsernamePassword,
+} from 'app/data/model/username-password.model';
+
