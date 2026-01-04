36a3cadddb92adc9b676bbf0be193295d5ac2b04
rezishon <rezashahmoradi.81@outlook.com>
2026-01-04 15:19:24 +0330

    feat(turn.model): add `Turn` zod const

--- src/app/services/turn-management/model/turn.model.ts
+++ src/app/services/turn-management/model/turn.model.ts
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 export interface Turn {
   TurnId: number;
   TurnIssueDate?: string;
@@ -11,3 +13,17 @@ export interface Turn {
   DateOfLastChanged?: string;
   SequentialTurnTitle?: string;
 }
+
+export const zodTurn = z.object({
+  TurnId: z.number(),
+  TurnIssueDate: z.string().optional(),
+  TurnIssueTime: z.string().optional(),
+  TruckDriver: z.string().optional(),
+  SoftwareUserName: z.string().optional(),
+  BillOfLadingNumber: z.string().optional(),
+  OtaghdarTurnNumber: z.string().optional(),
+  TurnStatusTitle: z.string().optional(),
+  TurnStatusDescription: z.string().optional(),
+  DateOfLastChanged: z.string().optional(),
+  SequentialTurnTitle: z.string().optional(),
+});
