bcb7ee9fcd944b80195ae9753612a4203374632c
rezishon <rezashahmoradi.81@outlook.com>
2026-01-04 13:05:36 +0330

    test(transport-companies-management): remove old test cases

--- src/app/services/transport-company-management/transport-companies-management.service.spec.ts
+++ src/app/services/transport-company-management/transport-companies-management.service.spec.ts
@@ -53,107 +53,15 @@ describe('TransportCompaniesManagementService', () => {
     devAuth.logout();
   });
 
-  it('GetTransportCompaniesInfo: should return array of transport companies', async () => {
-    await devAuth.loginAsAdmin();
-    const res = await service.GetTransportCompaniesInfo('ا');
-
-    expect(res.data)
-      .withContext(
-        'GetTransportCompaniesInfo: should return an array of transport companies'
-      )
-      .toEqual(jasmine.any(Array));
-  });
-
-  it('GetTransportCompanyInfo: should return single transport company', async () => {
-    await devAuth.loginAsAdmin();
-    const companyId = 21043;
-    const res = await service.GetTransportCompanyInfo(companyId);
-
-    expect(res.data)
-      .withContext(
-        'GetTransportCompanyInfo: should return transport company object'
-      )
-      .toBeDefined();
-  });
-
-  it('GetTransportCompanyBySoftwareUser: should return transport company', async () => {
     await devAuth.loginAsCompany();
-    const res = await service.GetTransportCompanyBySoftwareUser();
 
-    expect(res.data)
-      .withContext(
-        'GetTransportCompanyBySoftwareUser: should return transport company object'
-      )
-      .toBeDefined();
+
   });
 
-  it('EditTransportCompany: should return ShortResponse', async () => {
     await devAuth.loginAsAdmin();
-    const companyId = 21043;
-    const resData = await service.GetTransportCompanyInfo(companyId);
 
-    const res = await service.EditTransportCompany({
-      ...resData.data!,
-    });
 
-    expect(res.data)
-      .withContext('EditTransportCompany: should return response data')
-      .toBeDefined();
 
-    expect((res.data as ShortResponse)?.Message)
-      .withContext('EditTransportCompany: should return success message')
-      .toBeDefined();
-  });
 
-  // TODO: server return error موجودی کیف پول کافی نیست
-  xit('ActiveTransportCompanySmsService: should return ShortResponse', async () => {
-    await devAuth.loginAsAdmin();
-    const companyId = 21043;
-    const res = await service.ActiveTransportCompanySmsService(companyId);
-
-    expect(res.data)
-      .withContext(
-        'ActiveTransportCompanySmsService: should return response data'
-      )
-      .toBeDefined();
-
-    expect((res.data as ShortResponse)?.Message)
-      .withContext(
-        'ActiveTransportCompanySmsService: should return success message'
-      )
-      .toBeDefined();
-  });
-
-  it('ResetTransportCompanyPassword: should return username and password', async () => {
-    await devAuth.loginAsAdmin();
-    const res = await service.ResetTransportCompanyPassword(1);
-
-    expect(res.data)
-      .withContext('ResetTransportCompanyPassword: should return credentials')
-      .toBeDefined();
-
-    expect((res.data as UsernamePassword)?.Username)
-      .withContext('ResetTransportCompanyPassword: should return username')
-      .toBeDefined();
-
-    expect((res.data as UsernamePassword)?.Password)
-      .withContext('ResetTransportCompanyPassword: should return password')
-      .toBeDefined();
-  });
-
-  it('ChangeTransportCompanyStatus: should return ShortResponse', async () => {
-    await devAuth.loginAsAdmin();
-    const companyId = 21043;
-    const res = await service.ChangeTransportCompanyStatus(companyId, true);
-
-    expect(res.data)
-      .withContext('ChangeTransportCompanyStatus: should return response data')
-      .toBeDefined();
-
-    expect((res.data as ShortResponse)?.Message)
-      .withContext(
-        'ChangeTransportCompanyStatus: should return success message'
-      )
-      .toBeDefined();
   });
 });
