1e416083a8a83fefff400f30f560e27d8e197789
rezishon <rezashahmoradi.81@outlook.com>
2026-01-04 14:25:13 +0330

    feat(wallet-transaction.model): add `WalletTransaction` zod const

--- src/app/services/wallet-management/model/wallet-transaction.model.ts
+++ src/app/services/wallet-management/model/wallet-transaction.model.ts
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 export interface WalletTransaction {
   TransactionTitle: string;
   TransactionColor: string;
@@ -8,3 +10,14 @@ export interface WalletTransaction {
   Reminder: number;
   UserName: string;
 }
+
+export const zodWalletTransaction = z.object({
+  TransactionTitle: z.string(),
+  TransactionColor: z.string(),
+  ShamsiDate: z.string(),
+  Time: z.string(),
+  CurrentBalance: z.number(),
+  Amount: z.number(),
+  Reminder: z.number(),
+  UserName: z.string(),
+});
