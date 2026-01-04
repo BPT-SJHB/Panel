dff2231a4d2ab43fcb887455ffaa0e2425db5a63
rezishon <rezashahmoradi.81@outlook.com>
2026-01-04 14:24:37 +0330

    feat(wallet-payment-history.model): add `WalletPaymentHistory` zod const

--- src/app/services/wallet-management/model/wallet-payment-history.model.ts
+++ src/app/services/wallet-management/model/wallet-payment-history.model.ts
@@ -1,6 +1,15 @@
+import z from 'zod';
+
 export interface WalletPaymentHistory {
   ShamsiDate: string;
   Time: string;
   Amount: number;
   UserName: string;
 }
+
+export const zodWalletPaymentHistory = z.object({
+  ShamsiDate: z.string(),
+  Time: z.string(),
+  Amount: z.number(),
+  UserName: z.string(),
+});
