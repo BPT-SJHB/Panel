0ae302138874ef7ac1160fc6cff6a6b964ed8ab9
rezishon <rezashahmoradi.81@outlook.com>
2026-01-06 11:35:44 +0330

    test(lad-place-management): add file test + add imports

---
+++ src/app/services/lad-place-management/lad-place-management.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  createApiResponseSchema,
+  validateResponse,
+} from 'app/utils/validate-response.test.utils.spec';
+import { LADPlaceManagementService } from './lad-place-management.service';
+import { DevAuthService } from '../dev-auth-service/dev-auth.service';
+import { LADPlace, zodLADPlace } from 'app/data/model/lad-place.model';
+import z from 'zod';
+import { ShortResponse } from 'app/data/model/short-response.model';
+import { ApiShortResponseSchema } from 'app/data/model/short-response.model.spec';
+
