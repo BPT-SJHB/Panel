422f3361b9d1afbab00a0ec264ff5e93d77ede77
rezishon <rezashahmoradi.81@outlook.com>
2026-01-06 14:29:30 +0330

    feat(turn-status.model): add `TurnStatus` zod const

--- src/app/services/turn-management/model/turn-status.model.ts
+++ src/app/services/turn-management/model/turn-status.model.ts
@@ -1,4 +1,12 @@
+import z from 'zod';
+
 export interface TurnStatus {
-    TurnStatusId: number;
-    TurnStatusTitle: string;
+  TurnStatusId: number;
+  TurnStatusTitle: string;
 }
+
+export const zodTurnStatus = z.object({
+  TurnStatusId: z.number(),
+  TurnStatusTitle: z.string(),
+});
+
