df5a309e017cadb422fcd55449334ea3b9cbf09f
rezishon <rezashahmoradi.81@outlook.com>
2026-01-21 17:51:44 +0330

    feat(transport-tariff-param.model): add `TransportTariffParam` zod const

--- src/app/services/load-management/model/transport-tariff-param.model.ts
+++ src/app/services/load-management/model/transport-tariff-param.model.ts
@@ -1,6 +1,15 @@
+import { z } from 'zod';
+
 export interface TransportTariffParam {
   TPTPDId: number;
   TPTPTitle: string;
   Cost: number;
   Checked: boolean;
 }
+
+export const zodTransportTariffParam = z.object({
+  TPTPDId: z.number(),
+  TPTPTitle: z.string(),
+  Cost: z.number(),
+  Checked: z.boolean(),
+});
