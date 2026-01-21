b6eb3eab34506bed720995454bf6f0fb84530d7b
rezishon <rezashahmoradi.81@outlook.com>
2026-01-21 17:51:05 +0330

    feat(load-info.model): add `LoadInfo` zod const

--- src/app/services/load-management/model/load-info.model.ts
+++ src/app/services/load-management/model/load-info.model.ts
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 export interface LoadInfo {
   LoadId: number;
   AnnounceDate?: string;
@@ -30,3 +32,26 @@ export interface LoadInfo {
   TPTParams?: string;
   TPTParamsJoint?: string;
 }
+
+export const zodLoadInfo = z.object({
+  LoadId: z.number(),
+  AnnounceDate: z.string().optional(),
+  AnnounceTime: z.string().optional(),
+  TransportCompanyId: z.number().optional(),
+  TransportCompanyTitle: z.string().optional(),
+  GoodId: z.number().optional(),
+  GoodTitle: z.string().optional(),
+  AnnouncementGroupId: z.number().optional(),
+  AnnouncementGroupTitle: z.string().optional(),
+  AnnouncementSubGroupId: z.number().optional(),
+  AnnouncementSubGroupTitle: z.string().optional(),
+  SourceCityId: z.number().optional(),
+  SourceCityTitle: z.string().optional(),
+  TargetCityId: z.number().optional(),
+  TargetCityTitle: z.string().optional(),
+  LoadingPlaceId: z.number().optional(),
+  LoadingPlaceTitle: z.string().optional(),
+  DischargingPlaceId: z.number().optional(),
+  DischargingPlaceTitle: z.string().optional(),
+  TotalNumber: z.number().optional(),
+});
