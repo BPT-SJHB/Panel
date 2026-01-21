450065e5a639e218c5fb8918d2a6acd4301c357e
rezishon <rezashahmoradi.81@outlook.com>
2026-01-21 17:50:34 +0330

    feat(load-info-for... .model): add `LoadInfoForTransportCompanies_Factories_Admins_Drivers` zod const

--- src/app/services/load-management/model/load-info-for-transport-companies-factories-admins-drivers.model.ts
+++ src/app/services/load-management/model/load-info-for-transport-companies-factories-admins-drivers.model.ts
@@ -1,8 +1,4 @@
-export interface LoadForTransportCompanies_Factories_Admins_Drivers {
-  ProvinceName: string;
-  ProvinceId: number;
-  myLoads: LoadInfoForTransportCompanies_Factories_Admins_Drivers[];
-}
+import z from 'zod';
 
 export interface LoadInfoForTransportCompanies_Factories_Admins_Drivers {
   LoadId: number;
@@ -28,3 +24,30 @@ export interface LoadInfoForTransportCompanies_Factories_Admins_Drivers {
   UserName: string;
   TPTParamsJoint: string;
 }
+
+export const zodLoadInfoForTransportCompanies_Factories_Admins_Drivers =
+  z.object({
+    LoadId: z.number(),
+    TCTitle: z.string(),
+    GoodTitle: z.string(),
+    Tonaj: z.number(),
+    SoureCityTitle: z.string(),
+    TargetCityTitle: z.string(),
+    ProvinceId: z.number(),
+    LoadingPlaceTitle: z.string(),
+    DischargingPlaceTitle: z.string(),
+    Total: z.number(),
+    Reminder: z.number(),
+    Tariff: z.string(),
+    LoadStatusName: z.string(),
+    AnnounceDate: z.string(),
+    AnnounceTime: z.string(),
+    AnnouncementTitle: z.string(),
+    AnnouncementSGTitle: z.string(),
+    Recipient: z.string(),
+    Address: z.string(),
+    Description: z.string(),
+    UserName: z.string(),
+    TPTParamsJoint: z.string(),
+  });
+
