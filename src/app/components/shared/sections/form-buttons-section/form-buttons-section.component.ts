ebe346e172b9845c374b9289f6de557ad5fe7522
rezishon <rezashahmoradi.81@outlook.com>
2026-02-14 23:45:22 +0330

    feat(form-buttons-section.component): add specific div for form's buttons section

---
+++ src/app/components/shared/sections/form-buttons-section/form-buttons-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-buttons-section',
+  imports: [],
+  templateUrl: './form-buttons-section.component.html',
+  styleUrl: './form-buttons-section.component.scss',
+})
+export class FormButtonsSectionComponent {
+
+}
