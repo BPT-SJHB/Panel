6a7cf1375fbda768911828d43ba3596eb5cafb09
rezishon <rezashahmoradi.81@outlook.com>
2026-02-16 19:21:17 +0330

    feat(form-inputs-section.component): add toggleable card

--- src/app/components/shared/sections/form-inputs-section/form-inputs-section.component.ts
+++ src/app/components/shared/sections/form-inputs-section/form-inputs-section.component.ts
@@ -1,14 +1,24 @@
 import { Component, input } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common'; // <-- MUST IMPORT THIS
 import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-form-inputs-section',
-  imports: [CardModule, NgTemplateOutlet],
+  imports: [CardModule, NgTemplateOutlet, PanelModule],
   templateUrl: './form-inputs-section.component.html',
   styleUrl: './form-inputs-section.component.scss',
 })
 export class FormInputsSectionComponent {
   readonly flexDir = input<'flex-row' | 'flex-col'>('flex-col');
   readonly isCard = input<boolean>(true);
+  readonly header = input<string>('');
+  readonly ptValue = {
+    root: {
+      class: 'bg-stone-100 dark:bg-zinc-800 shadow-md border-0 rounded-xl',
+    },
+    title: {
+      class: 'w-full text-xl font-bold px-2 block',
+    },
+  };
 }
