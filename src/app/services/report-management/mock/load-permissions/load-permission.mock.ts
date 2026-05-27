aa4d983d386a331371fcaa3a51c7f854884ed6f0
rezishon <rezashahmoradi.81@outlook.com>
2026-05-27 22:18:07 +0330

    chore(load-permission.mock): add new record of mock

--- src/app/services/report-management/mock/load-permissions/load-permission.mock.ts
+++ src/app/services/report-management/mock/load-permissions/load-permission.mock.ts
@@ -33,4 +33,68 @@ export const mockLoadPermissions: LoadPermission[] = [
       'تایید شده و مجوز صادر شده                                                                           ',
     TPTParamsJoint: '0',
   },
+  {
+    LoadId: 16,
+    GoodTitle: 'شمش چدن',
+    LoadSourceCity: 'اصفهان - ذوب آهن',
+    LoadTargetCity: 'مشگين شهر',
+    AnnouncementTitle:
+      'تريلي برون شهري                                                                                     ',
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    TransportCompany: 'ا مانت  بار                        ',
+    Recipient: '',
+    Address: '',
+    Description: 'فرداصبح بارگيري',
+    LoadRegisteringUser:
+      'مرتضي شاهمرادي                                                                                      ',
+    LoadAllocationUser:
+      'حسن;عباسي                                                                                           ',
+    LoadAllocationId: 30030,
+    LicensePlate: '286ع11 - 13',
+    SmartCardNo: '15161718',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    NationalCode: '1110253257',
+    MobileNumber: '09131767833',
+    ShamsiDate: '1404/05/16',
+    Time: '21:46:11  ',
+    SequentialTurn:
+      'T1404/000007                                                                                        ',
+    Note: '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ',
+    LoadAllocationStatusTitle:
+      'تایید شده و مجوز صادر شده                                                                           ',
+    TPTParamsJoint: '0',
+  },
+  {
+    LoadId: 17,
+    GoodTitle: 'شمش چدن',
+    LoadSourceCity: 'اصفهان - ذوب آهن',
+    LoadTargetCity: 'مشگين شهر',
+    AnnouncementTitle:
+      'تريلي برون شهري                                                                                     ',
+    AnnouncementSGTitle:
+      'برون شهری آهن آلات ذوبی                                                                             ',
+    TransportCompany: 'ا مانت  بار                        ',
+    Recipient: '',
+    Address: '',
+    Description: 'فرداصبح بارگيري',
+    LoadRegisteringUser:
+      'مرتضي شاهمرادي                                                                                      ',
+    LoadAllocationUser:
+      'حسن;عباسي                                                                                           ',
+    LoadAllocationId: 30031,
+    LicensePlate: '286ع11 - 13',
+    SmartCardNo: '15161718',
+    TruckDriver: ';;;محمد بابائي بندارتي',
+    NationalCode: '1110253257',
+    MobileNumber: '09131767833',
+    ShamsiDate: '1404/05/16',
+    Time: '21:46:11  ',
+    SequentialTurn:
+      'T1404/000007                                                                                        ',
+    Note: '                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ',
+    LoadAllocationStatusTitle:
+      'تایید شده و مجوز صادر شده                                                                           ',
+    TPTParamsJoint: '0',
+  },
 ];
