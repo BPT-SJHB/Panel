5863a410e9b9fbcc0cd1f6a79f996df9ae4bd514
rezishon <rezashahmoradi.81@outlook.com>
2026-07-05 17:57:18 +0330

    ref(services/location-management): update `LocationManagement`
    Service now cover our routine code styling.
    Error and toast handling inside components.
    Services only return values.

--- src/app/services/location-management/location-management.service.ts
+++ src/app/services/location-management/location-management.service.ts
@@ -1,108 +1,58 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+export interface ILocation {
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocationManagementService {
-  isLoading = signal<boolean>(false);
-  error = signal<string | null>(null);
-  currentLocation = signal<unknown | null>(null);
+  async checkPermissionStatus() {
+    let permissionStatus: unknown;
 
-  permissionStatus = signal<PermissionState | null>(null);
-
-  constructor() {
-    this.checkPermissionStatus();
-  }
-
-  private async checkPermissionStatus() {
-    if (!navigator.permissions) return;
+    if (!navigator.permissions) {
+      return 'Permissions API not supported by your browser.';
+    }
 
     try {
       const status = await navigator.permissions.query({ name: 'geolocation' });
-      this.permissionStatus.set(status.state);
+      permissionStatus = status.state;
 
       status.onchange = () => {
-        this.permissionStatus.set(status.state);
+        permissionStatus = status.state;
       };
+
+      return permissionStatus;
     } catch (err) {
-      console.error(
-        'Permissions API not supported for geolocation in this browser.'
+      return (
+        'Permissions API not supported for geolocation in this browser.' + err
       );
     }
   }
 
-  fetchUserLocation() {
-    this.isLoading.set(true);
-    this.error.set(null);
-
+  async fetchUserLocation(): Promise<ILocation | null> {
     if (!navigator.geolocation) {
-      alert('Geolocation is not supported by your browser.');
-      this.isLoading.set(false);
-      return;
+      return null;
     }
 
-    alert('Geolocation is supported by your browser.');
-
-    // 2. Request the location
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        const locationPayload = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          accuracy: position.coords.accuracy,
-          altitude: position.coords.altitude,
-          heading: position.coords.heading,
-          speed: position.coords.speed,
-          timestamp: position.timestamp,
-        };
-
-        this.currentLocation.set(locationPayload);
-        this.isLoading.set(false);
-
-        this.sendToApi({
-          latitude: locationPayload.latitude,
-          longitude: locationPayload.longitude,
-          accuracy: locationPayload.accuracy,
-        });
-      },
-      (err: GeolocationPositionError) => {
-        this.isLoading.set(false);
-        this.handleLocationError(err);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 30000,
-        maximumAge: 0,
-      }
-    );
-  }
-
-  private sendToApi(payload: {
-    latitude: unknown;
-    longitude: unknown;
-    accuracy: unknown;
-  }) {
-    // Example API call
-
-    alert(
-      `The GPS lat:${payload.latitude} long:${payload.longitude} accuracy:${payload.accuracy}`
-    );
-  }
-
-  private handleLocationError(error: GeolocationPositionError) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        alert('User denied the request for Geolocation.');
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert('Location information is unavailable.');
-        break;
-      case error.TIMEOUT:
-        alert('The request to get user location timed out.');
-        break;
-      default:
-        alert('An unknown error occurred.');
-        break;
-    }
+    return new Promise((resolve) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const locationPayload: ILocation = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            accuracy: position.coords.accuracy,
+          };
+          resolve(locationPayload);
+        },
+        (_) => {
+          resolve(null);
+        },
+        { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 }
+      );
+    });
   }
 }
